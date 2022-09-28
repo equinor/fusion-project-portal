5b5528e1bf04c0abf4d76bf9df2bde5c1109bd78
Christopher Berge Hove <chbh@equinor.com>
2022-09-28 14:51:26 +0200

    fix(documentation): fix documentation build (#4)
    # Description
    
    - renamed documentation project, removed double fusion
    - fix build by removing empty tags and setting build root to docs
    - added sidebar to backend 
    
    
    Completes: AB#FILL_IN_YOUR_ISSUE_ID
    
    ## Type of change
    
    Remove options that are not relevant.
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide instructions so we can reproduce. Please also list any relevant details for your test configuration
    
    ## Checklist
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code.
    - [ ] I have linked my DevOps task using the AB# tag.
    - [ ] My code is easy to read.
    - [ ] Documentation and comments is added where needed.
    - [ ] My code is covered by tests.
    - [ ] UX has reviewed relevant UI contributions.

--- documentation/src/.vuepress/config.ts
+++ documentation/src/.vuepress/config.ts
@@ -10,7 +10,7 @@ export default hope.config({
     repo: "equinor/fusion-project-portal",
     docsBranch: "main",
     editLinks: true,
-    docsDir: "docs/src",
+    docsDir: "docs/",
     darkmode: "auto-switch",
     comment: false,
     nav: [
@@ -27,6 +27,20 @@ export default hope.config({
       {
         title: "Frontend",
         path: "/frontend/",
+        collapsable: false,
+        sidebarDepth: 3,
+        children: [
+          // {
+          // 	title: 'Section',
+          // 	path: '/frontend/section',
+          // 	children: [
+          // 	],
+          // },
+        ],
+      },
+      {
+        title: "Backend",
+        path: "/backend/",
         collapsable: false,
         sidebarDepth: 3,
         children: [
