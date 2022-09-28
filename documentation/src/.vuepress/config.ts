48ccec98817fb961f272d8ee4e530174cd8d7f85
Christopher Berge Hove <chbh@equinor.com>
2022-09-28 14:46:01 +0200

    added sidebar to vuepress config

--- documentation/src/.vuepress/config.ts
+++ documentation/src/.vuepress/config.ts
@@ -10,7 +10,7 @@ export default hope.config({
     repo: "equinor/fusion-project-portal",
     docsBranch: "main",
     editLinks: true,
-    docsDir: "docs/src",
+    docsDir: "docs/",
     darkmode: "auto-switch",
     comment: false,
     nav: [
@@ -23,6 +23,37 @@ export default hope.config({
         link: "/backend/",
       },
     ],
+    sidebar: [
+      {
+        title: "Frontend",
+        path: "/frontend/",
+        collapsable: false,
+        sidebarDepth: 3,
+        children: [
+          // {
+          // 	title: 'Section',
+          // 	path: '/frontend/section',
+          // 	children: [
+          // 	],
+          // },
+        ],
+      },
+      {
+        title: "Backend",
+        path: "/backend/",
+        collapsable: false,
+        sidebarDepth: 3,
+        children: [
+          // {
+          // 	title: 'Section',
+          // 	path: '/frontend/section',
+          // 	children: [
+          // 	],
+          // },
+        ],
+      },
+    ],
+
     mdEnhance: {
       mermaid: true,
       codegroup: true,
