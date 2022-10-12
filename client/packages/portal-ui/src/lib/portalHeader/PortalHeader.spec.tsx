6705e11f5d6c85b527a8b7a5e2066cbbe38bfeb6
Christopher Berge Hove <chbh@equinor.com>
2022-10-12 11:53:28 +0200

    Configure jest for testing

--- client/packages/portal-ui/src/lib/portalHeader/PortalHeader.spec.tsx
+++ client/packages/portal-ui/src/lib/portalHeader/PortalHeader.spec.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@equinor/eds-core-react';
 import { render } from '@testing-library/react';
 
-import PortalHeader from './portalHeader';
+import PortalHeader from './PortalHeader';
 
 describe('PortalTopBar', () => {
   it('should render successfully', () => {
