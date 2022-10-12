4ab68b1c92dc3cb2d4369f422d648136ac563cff
Kjetil Haugland <801342+kjetilhau@users.noreply.github.com>
2022-10-12 15:58:00 +0200

    Added WorkSurface controller

---
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/WorkSurfaceController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Equinor.ProjectExecutionPortal.WebApi.Controllers
+{
+    public class WorkSurfaceController : Controller
+    {
+        [HttpGet("/work-surface/apps")]
+        public IActionResult Apps()
+        {
+            return Json("yo");
+        }
+    }
+}
