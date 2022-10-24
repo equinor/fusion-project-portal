ea5c8251c2caca1cbfc29662aaa0e99890655746
Christopher Berge Hove <chbh@equinor.com>
2022-10-24 11:59:31 +0200

    chore(refactor): added back vite alias for resoling packages

--- client/packages/portal-client/vite.config.ts
+++ client/packages/portal-client/vite.config.ts
@@ -1,6 +1,8 @@
 import react from '@vitejs/plugin-react';
+import path from "path";
 import { defineConfig } from 'vite';
 import tsconfig from 'vite-tsconfig-paths';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +22,26 @@ export default defineConfig({
           return `/${pathArray[2]}.js`;
         },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@equinor/portal-core': path.resolve(
+        __dirname,
+        '../portal-core/src/index.ts'
+      ),
+      '@equinor/portal-pages': path.resolve(
+        __dirname,
+        '../portal-pages/src/index.ts'
+      ),
+      '@equinor/portal-ui': path.resolve(
+        __dirname,
+        '../portal-ui/src/index.ts'
+      ),
+      '@equinor/portal-utils': path.resolve(
+        __dirname,
+        '../portal-utils/src/index.ts'
+      ),
     },
   },
 });
