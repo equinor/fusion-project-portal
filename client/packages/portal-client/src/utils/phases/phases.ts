10d3b8194f1b005884ae9ac6f1b3e7eedb04f029
Gustav-Eikaas <guei@equinor.com>
2022-10-24 13:00:39 +0200

    docs: :memo: add tsdoc to phase

--- client/packages/portal-client/src/utils/phases/phases.ts
+++ client/packages/portal-client/src/utils/phases/phases.ts
@@ -3,8 +3,10 @@ import { storage } from '@equinor/portal-utils';
 import { BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+//Key the selected phase is stored under
 const storageKey = 'selectedPhase';
 
+//Interface to read and save phase from local storage
 export const phaseStorage = {
   readPhase: () => storage.getItem(storageKey) as string | undefined,
   storePhase: (id: string | undefined) => storage.setItem(storageKey, id),
@@ -16,5 +18,16 @@ const phaseSubject = new BehaviorSubject<string | undefined>(
 
 export const selectedPhase$ = phaseSubject.pipe(tap(phaseStorage.storePhase));
 
+/**
+ * Clear selected phase
+ *
+ * Sends user back to homepage
+ */
 export const clearSelectedPhase = () => phaseSubject.next(undefined);
+
+/**
+ * Set active phase
+ *
+ * Used for navigating a user to a certain phase homepage
+ */
 export const setActivePhase = (phase: Phase) => phaseSubject.next(phase.id);
