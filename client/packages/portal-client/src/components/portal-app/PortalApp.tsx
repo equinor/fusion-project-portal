d71495d1a63a565fd2f0c044f7d986035e82a180
Gustav-Eikaas <guei@equinor.com>
2022-10-24 12:57:59 +0200

    chore: :fire: remove unused hook

--- client/packages/portal-client/src/components/portal-app/PortalApp.tsx
+++ client/packages/portal-client/src/components/portal-app/PortalApp.tsx
@@ -1,12 +1,6 @@
 import { MenuProvider, PortalMenu, StyleProvider } from '@equinor/portal-ui';
-import { phases } from 'packages/portal-core/src/mock.ts/phases';
 import { Phase } from '../phase/Phase';
-import { useQuery } from 'react-query';
 import Header from '../portal-header/Header';
-import { useEffect } from 'react';
-import { storage } from '@equinor/portal-utils';
-import { phaseStorage, setActivePhase } from '../../utils/phases/phases';
-import { Phase as IPhase } from '@equinor/portal-core';
 
 export function PortalApp() {
   return (
@@ -21,14 +15,3 @@ export function PortalApp() {
 }
 
 export default PortalApp;
-
-export function useInitPhase() {
-  useQuery<IPhase[]>(['phases', () => Promise.resolve(phases)], {
-    onSuccess: (data) => {
-      const id = phaseStorage.readPhase();
-      const phase = data.find((s) => s.id === id);
-      if (!phase) return;
-      setActivePhase(phase);
-    },
-  });
-}
