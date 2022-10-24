76f9080e59651a2d14a2f52b31f8474a689b5b25
Gustav-Eikaas <89254170+Gustav-Eikaas@users.noreply.github.com>
2022-10-24 11:07:45 +0200

    chore: :wrench: vite read import paths from tsconfig (#13)

--- client/packages/portal-client/vite.config.ts
+++ client/packages/portal-client/vite.config.ts
@@ -1,11 +1,10 @@
 import react from '@vitejs/plugin-react';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
+import tsconfig from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react(), tsconfig()],
   preview: { port: 3000 },
 
   server: {
@@ -21,14 +20,6 @@ export default defineConfig({
           return `/${pathArray[2]}.js`;
         },
       },
-    },
-  },
-  resolve: {
-    alias: {
-      '@equinor/portal-ui': path.resolve(
-        __dirname,
-        '../portal-ui/src/index.ts'
-      ),
     },
   },
 });
