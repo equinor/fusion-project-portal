d1627b9d38af1d52a221e405f2806a1ef81d7f1f
Gustav-Eikaas <guei@equinor.com>
2022-10-25 09:08:07 +0200

    groups matching search

--- client/packages/portal-client/src/components/portal-menu/PortalMenu.tsx
+++ client/packages/portal-client/src/components/portal-menu/PortalMenu.tsx
@@ -1,8 +1,78 @@
+import { Search } from '@equinor/eds-core-react';
 import { PortalMenu } from '@equinor/portal-ui';
+import { useObservable } from '@equinor/portal-utils';
+import { BehaviorSubject, combineLatestWith, map } from 'rxjs';
+
+const MOCK_GROUPS: MenuGroup[] = [
+  {
+    name: 'Project Information',
+    children: ['Home', 'Business analytics', 'Organization chart'],
+    color: '#6D2FD5',
+  },
+  {
+    name: 'SSU',
+    color: '#E24973',
+    children: ['Barrier management', 'Safety performance'],
+  },
+  {
+    name: 'Engineering',
+    color: ' #00977B',
+    children: ['LCI Hanging garden', 'MDR analytics'],
+  },
+  {
+    name: 'Quality and Risk',
+    children: ['Non Conformity'],
+    color: '#FF92A8',
+  },
+  {
+    name: 'Construction & Commissioning',
+    color: '#0084C4',
+    children: [
+      'Handover',
+      'Project Browser',
+      'Work preparation',
+      'Installation',
+      'Mechanical Completion',
+      'Piping and Heat trace',
+      'Preservation',
+      'Checklist',
+      'SWCR',
+      'Invitation to Punch Out',
+      'Commissioning Procedure',
+    ],
+  },
+];
+
+const groups$ = new BehaviorSubject<MenuGroup[]>(MOCK_GROUPS);
+const search$ = new BehaviorSubject<string | undefined>(undefined);
+const groupsMatchingSearch$ = groups$.pipe(
+  combineLatestWith(search$.pipe(map((s) => s?.toLowerCase()))),
+  map(([groups, search]) => {
+    if (!search) return groups;
+
+    return groups
+      .map((group) => ({
+        ...group,
+        children: group.children.filter((group) =>
+          group.toLowerCase().includes(search)
+        ),
+      }))
+      .filter((group) => group.children.length);
+  })
+);
 
 export function MenuGroups() {
+  const groups = useObservable(groupsMatchingSearch$);
+
+  if (!groups) return null;
+
   return (
     <PortalMenu>
+      <Search
+        onChange={(e) => {
+          search$.next(e.target.value);
+        }}
+      />
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5em' }}>
         {groups.map((group) => (
           <Group key={group.name} group={group} />
@@ -52,43 +122,3 @@ type MenuGroup = {
   color: string;
   children: string[];
 };
-
-const groups: MenuGroup[] = [
-  {
-    name: 'Project Information',
-    children: ['Home', 'Business analytics', 'Organization chart'],
-    color: '#6D2FD5',
-  },
-  {
-    name: 'SSU',
-    color: '#E24973',
-    children: ['Barrier management', 'Safety performance'],
-  },
-  {
-    name: 'Engineering',
-    color: ' #00977B',
-    children: ['LCI Hanging garden', 'MDR analytics'],
-  },
-  {
-    name: 'Quality and Risk',
-    children: ['Non Conformity'],
-    color: '#FF92A8',
-  },
-  {
-    name: 'Construction & Commissioning',
-    color: '#0084C4',
-    children: [
-      'Handover',
-      'Project Browser',
-      'Work preparation',
-      'Installation',
-      'Mechanical Completion',
-      'Piping and Heat trace',
-      'Preservation',
-      'Checklist',
-      'SWCR',
-      'Invitation to Punch Out',
-      'Commissioning Procedure',
-    ],
-  },
-];
