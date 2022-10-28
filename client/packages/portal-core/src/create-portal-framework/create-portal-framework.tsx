a2ae6def4c0ba0a359d9d7dbef34e4eb26be558c
Gustav-Eikaas <guei@equinor.com>
2022-10-28 14:47:45 +0200

    refactor: :fire: remove phase module

--- client/packages/portal-core/src/create-portal-framework/create-portal-framework.tsx
+++ client/packages/portal-core/src/create-portal-framework/create-portal-framework.tsx
@@ -1,19 +1,19 @@
 import { configureAgGrid } from '@equinor/fusion-framework-module-ag-grid';
 import { ConsoleLogger } from '@equinor/fusion-framework-module-msal/client';
 import { createFrameworkProvider } from '@equinor/fusion-framework-react';
+import { BehaviorSubject } from 'rxjs';
 
 import { configureModuleLoader } from '../module-loader/module';
-import { LoggerLevel, Phase, PortalConfig } from '../types/portal-config';
+import { LoggerLevel, PortalConfig } from '../types/portal-config';
 
-// { name: 'phase'; initialize: () => { phases: Phase[] } }
+export const framework$ = new BehaviorSubject<null | any>(null);
+
 export function createPortalFramework(
   portalConfig: PortalConfig
 ): React.LazyExoticComponent<
   React.FunctionComponent<{ children?: React.ReactNode }>
 > {
-  return createFrameworkProvider<
-    [{ name: 'phase'; initialize: () => { phases: Phase[] } }]
-  >((config) => {
+  return createFrameworkProvider((config) => {
     config.logger.level = (portalConfig.logger?.level as LoggerLevel) || 0;
 
     config.configureServiceDiscovery(portalConfig.serviceDiscovery);
@@ -24,17 +24,8 @@ export function createPortalFramework(
       config.addConfig(configureAgGrid(portalConfig.agGrid));
     }
 
-    config.onConfigured(() => {
+    config.onConfigured((s) => {
       console.log('framework config done');
-    });
-
-    config.addConfig({
-      module: {
-        name: 'phase',
-        initialize: () => ({
-          phases: portalConfig.phases || [],
-        }),
-      },
     });
 
     config.addConfig(
@@ -44,11 +35,11 @@ export function createPortalFramework(
     );
 
     config.onInitialized(async (fusion) => {
+      framework$.next(fusion);
       if (portalConfig.logger?.defaultClientLogger?.active) {
         fusion.auth.defaultClient.setLogger(
           new ConsoleLogger(portalConfig.logger.defaultClientLogger.level)
         );
-
         console.debug('📒 subscribing to all events');
         fusion.event.subscribe((e) => console.debug(`🔔🌍 [${e.type}]`, e));
 
