6a8b78730a8811d229f9c0bf6015318bcd0d0556
Gustav-Eikaas <guei@equinor.com>
2022-11-02 09:01:13 +0100

    refactor: :recycle: refactor menu items

--- client/packages/portal-core/src/hooks/use-menu-items.ts
+++ client/packages/portal-core/src/hooks/use-menu-items.ts
@@ -1,3 +1,4 @@
+import { useHttpClient } from '@equinor/fusion-framework-react/hooks';
 import { useQuery } from 'react-query';
 import { from, lastValueFrom, map, switchMap } from 'rxjs';
 import { requirePortalClient } from '../clients';
@@ -8,18 +9,19 @@ import { useCurrentWorkSurface, WorkSurface } from '../work-surface-module';
  */
 export const useMenuItems = () => {
   const surface = useCurrentWorkSurface();
+
+  const client = useHttpClient('portal-client' as 'portal');
+
   return useQuery(
     ['menu-items', surface?.id],
     async () => {
-
       if (surface?.id) {
         return lastValueFrom(
-          from(requirePortalClient()).pipe(
-            switchMap((s) =>
-              s.fetch$(
-                `https://app-pep-backend-noe-dev.azurewebsites.net/api/work-surfaces/${surface.id}`
-              )
-            ),
+          from(
+            client.fetch$(
+              `https://app-pep-backend-noe-dev.azurewebsites.net/api/work-surfaces/${surface.id}`
+            )
+          ).pipe(
             switchMap((res) => res.json()),
             map((workSurface: WorkSurface) => workSurface.appGroups)
           )
