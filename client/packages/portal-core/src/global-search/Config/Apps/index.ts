5184612c4e3599d5b7933253ca5b05868096e9f2
Christopher Berge Hove <chbh@equinor.com>
2022-11-02 10:17:34 +0100

    chore: :fire: removed global serch code addeed prematurely

--- client/packages/portal-core/src/global-search/Config/Apps/index.ts
+++
@@ -1,8 +0,0 @@
-import { appsSearchMapper } from './appsSearchMapper';
-import { appsSearchRequest } from './appsSearchRequest';
-
-export const appsConfig = {
-    type: 'apps',
-    searchRequest: appsSearchRequest,
-    searchMapper: appsSearchMapper,
-};
--- client/packages/portal-core/src/global-search/Config/ProCoSys/index.ts
+++
@@ -1,8 +0,0 @@
-import { proCoSysSearchMapper } from './proCoSysSearchMapper';
-import { proCoSysSearchRequest } from './proCoSysSearchRequest';
-
-export const procosysConfig = {
-    type: 'procosys',
-    searchRequest: proCoSysSearchRequest,
-    searchMapper: proCoSysSearchMapper,
-};
