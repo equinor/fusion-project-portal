6d9e1ffccee727fd01984af1157cdfd4f6492fdc
Gustav-Eikaas <guei@equinor.com>
2022-11-03 12:42:07 +0100

    fix: :bug: fix merge error

--- client/packages/portal-core/src/index.ts
+++ client/packages/portal-core/src/index.ts
@@ -3,3 +3,4 @@ export * from './hooks';
 export * from './work-surface-module';
 export * from './portal-framework-config/portal-framework-config';
 export * from './types/portal-config';
+export * from './menu';
