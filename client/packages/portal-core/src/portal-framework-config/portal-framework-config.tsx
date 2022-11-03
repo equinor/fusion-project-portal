fc97bf7afb74ed7e95b172f55788d1559067e047
Christopher Berge Hove <chbh@equinor.com>
2022-11-03 11:16:32 +0100

    feat: :sparkles: added project portal configurator  (#28)

---
+++ client/packages/portal-core/src/portal-framework-config/portal-framework-config.tsx
@@ -0,0 +1,47 @@
+import { ConsoleLogger } from '@equinor/fusion-framework-module-msal/client';
+
+import { BehaviorSubject } from 'rxjs';
+
+import { ProjectPortalConfigurator } from '../portal-framework-configurator/portal-framework-configurator';
+import { LoggerLevel, PortalConfig } from '../types/portal-config';
+
+export const framework$ = new BehaviorSubject<null | any>(null);
+
+export function createPortalFramework(portalConfig: PortalConfig) {
+  return (config: ProjectPortalConfigurator) => {
+    config.logger.level = (portalConfig.logger?.level as LoggerLevel) || 0;
+
+    config.configureServiceDiscovery(portalConfig.serviceDiscovery);
+
+    config.configureMsal(portalConfig.masal.client, portalConfig.masal.options);
+
+    if (portalConfig.agGrid) {
+      config.configureAgGrid(portalConfig.agGrid);
+    }
+
+    config.configurePortalClient(portalConfig.portalClient.client);
+
+    config.configureAppLoader((moduleId: string) => {
+      return 'https://app-pep-backend-noe-dev.azurewebsites.net/api/bundles/test-app.js';
+    });
+
+    config.onConfigured(() => {
+      console.log('framework config done');
+    });
+
+    config.onInitialized(async (fusion) => {
+      framework$.next(fusion);
+      fusion.auth.defaultClient.setLogger(new ConsoleLogger(0));
+
+      console.debug('📒 subscribing to all events');
+      fusion.event.subscribe((e) => console.debug(`🔔🌍 [${e.type}]`, e));
+
+      console.debug('📒 subscribing to [onReactAppLoaded]');
+      fusion.event.addEventListener('onReactAppLoaded', (e) =>
+        console.debug('🔔 [onReactAppLoaded]', e)
+      );
+    });
+  };
+}
+
+export default createPortalFramework;
