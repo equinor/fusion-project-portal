f81c909fef0074def25f57c6e96f276c324c4d29
Kjetil Haugland <801342+kjetilhau@users.noreply.github.com>
2022-11-14 15:16:09 +0100

    Updated packages to latest

--- backend/src/Equinor.ProjectExecutionPortal.WebApi/Behaviors/LoggingBehavior.cs
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/Behaviors/LoggingBehavior.cs
@@ -9,7 +9,7 @@ public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
 
     public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger) => _logger = logger;
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         _logger.LogInformation("----- Handling command {CommandName} ({@Command})", request.GetGenericTypeName(), request);
         var response = await next();
