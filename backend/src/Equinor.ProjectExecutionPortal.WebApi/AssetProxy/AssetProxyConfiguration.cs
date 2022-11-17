04b1e934a4c93f4f75130a8c0944adcfa75e1782
Kjetil Haugland <801342+kjetilhau@users.noreply.github.com>
2022-11-17 10:27:04 +0100

    PR fixes

--- backend/src/Equinor.ProjectExecutionPortal.WebApi/AssetProxy/AssetProxyConfiguration.cs
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/AssetProxy/AssetProxyConfiguration.cs
@@ -17,9 +17,9 @@
 
         public static IEndpointRouteBuilder MapFusionPortalAssetProxy(this IEndpointRouteBuilder endpoints)
         {
-            endpoints.MapGet("/_assets/legacy-apps/{appKey}.js", AssetProxyHandler.ProxyRequestAsync<LegacyAppRequestTransformer>);
-            endpoints.MapGet("/_assets/legacy-resources/{appKey}/{*resourcePath}", AssetProxyHandler.ProxyRequestAsync<LegacyAppResourcesRequestTransformer>);
-            endpoints.MapGet("/_assets/images/profiles/{uniqueId}", AssetProxyHandler.ProxyRequestAsync<ProfileImageRequestTransformer>);
+            endpoints.MapGet("/assets/legacy-apps/{appKey}.js", AssetProxyHandler.ProxyRequestAsync<LegacyAppRequestTransformer>);
+            endpoints.MapGet("/assets/legacy-resources/{appKey}/{*resourcePath}", AssetProxyHandler.ProxyRequestAsync<LegacyAppResourcesRequestTransformer>);
+            endpoints.MapGet("/assets/images/profiles/{uniqueId}", AssetProxyHandler.ProxyRequestAsync<ProfileImageRequestTransformer>);
             endpoints.MapGet("/images/profiles/{uniqueId}", AssetProxyHandler.ProxyRequestAsync<ProfileImageRequestTransformer>);
 
             return endpoints;
