8e0f7cec12a4a9538cc16cd86b9d76fe04aa97da
Christopher Berge Hove <chbh@equinor.com>
2022-11-29 10:40:24 +0100

    chore: structure and style extractions

--- client/packages/portal-ui/src/lib/side-sheet-header.tsx/SideSheetHeader.tsx
+++ client/packages/portal-ui/src/lib/side-sheet-header.tsx/SideSheetHeader.tsx
@@ -1,45 +1,17 @@
 import { Button, Icon, Typography } from '@equinor/eds-core-react';
-import { tokens } from '@equinor/eds-tokens';
 import { usePortalActions } from '@equinor/portal-core';
 import { PropsWithChildren, useRef } from 'react';
-import styled from 'styled-components';
 import { FullscreenIcon } from '../fullscreen-icon/FullscreenIcon';
-
-const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: ${tokens.colors.ui.background__default.hex};
-`;
-
-const StyledHeader = styled.div`
-  padding: 1rem;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const StyledTitleContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding-left: 1rem;
-`;
-
-const StyledTitleWrapper = styled.div`
-  display: flex;
-`;
-
-const StyledActionsWrapper = styled.div`
-  display: flex;
-`;
-
-type HEXString = `#${string}`;
-
-const StyledIndicator = styled.span<{ color?: HEXString }>`
-  background-color: ${({ color }) => color || 'red'};
-  height: 48px;
-  width: 16px;
-`;
-
-const ContentWrapper = styled.div``;
+import {
+  ContentWrapper,
+  StyledActionsWrapper,
+  StyledHeader,
+  StyledIndicator,
+  StyledTitleContentWrapper,
+  StyledTitleWrapper,
+  StyledWrapper,
+} from './side-sheet-header-styles';
+import { HEXString } from './types';
 
 export function SideSheetHeader({
   children,
