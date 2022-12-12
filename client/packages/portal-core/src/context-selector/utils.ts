ff35d90dbd19b48a82e59c8f9f286287178ceefd
Christopher Berge Hove <christopher.hove@bouvet.no>
2022-12-12 11:24:14 +0100

    added util for context url and test

---
+++ client/packages/portal-core/src/context-selector/utils.ts
@@ -0,0 +1,8 @@
+export function getContextPageUrl(contextId?: string): string {
+  if (!contextId) return "context-page/"
+
+  const params = new URLSearchParams()
+  params.append("contextId", contextId)
+  
+  return `context-page/$${params.toString()}`
+} 
