3708bdb4fbfda8cb5b080c4d19d6eaf40feb5358
Christopher Berge Hove <christopher.hove@bouvet.no>
2022-12-12 11:24:51 +0100

    chore: update context selector

--- client/packages/portal-core/src/context-selector/PortalContextSelector.tsx
+++ client/packages/portal-core/src/context-selector/PortalContextSelector.tsx
@@ -1,8 +1,5 @@
 import { Button } from '@equinor/eds-core-react';
-import ContextSelector, {
-  ContextResult,
-} from '@equinor/fusion-react-context-selector';
-import { useRef } from 'react';
+import ContextSelector from '@equinor/fusion-react-context-selector';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import {
@@ -10,6 +7,7 @@ import {
   useFrameworkContext,
   useFrameworkCurrentContext,
 } from '../hooks';
+import { getContextPageUrl } from './utils';
 
 /**
  * Context user flow
@@ -32,6 +30,7 @@ const StyledWrapper = styled.div`
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
+    gap: .5rem;
 `;
 
 export const PortalContextSelector = () => {
@@ -56,23 +55,19 @@ export const PortalContextSelector = () => {
         onSelect={(e: any) => {
           e.stopPropagation();
           contextProvider.contextClient.setCurrentContext(e.nativeEvent.detail.selected[0].id);
-          navigate(`context-page/$contextId=${e.nativeEvent.detail.selected[0].id}`);
+          navigate(getContextPageUrl(e.nativeEvent.detail.selected[0].id));
         }}
         value={currentContext?.id ? currentContext?.title || "" :  ''}
         placeholder="Start to type to search..."
-        onChange={(e) => {
-          e.stopPropagation();
-          console.log(e);
-        }}
+
       />
 
       { currentContext && (<Button variant='ghost' onClick={()=> {
-        navigate(`context-page/$contextId=${currentContext?.id}`)
+        navigate(getContextPageUrl(currentContext?.id))
       }}>
         Go to project
       </Button>)}
     </StyledWrapper>
   );
 };
-
 
--- client/packages/portal-core/src/framework-configurator/portal-context-configurators.ts
+++ client/packages/portal-core/src/framework-configurator/portal-context-configurators.ts
@@ -11,9 +11,8 @@ export function storeCurrentContext(contextProvider: IContextProvider) {
 
     // Update the history with the current context selected.
     setContextHistory(context);
-    console.log("storeCurrentContext", context, storedContextId)
+
     if (context.id !== storedContextId){
-      console.log("set context to store")
       storage.setItem(CONTEXT_SOCAGE_KEY, context?.id);
     }
   });
@@ -23,12 +22,9 @@ export function setStoredContext(contextProvider: IContextProvider) {
   const storedContextId = storage.getItem<string>(CONTEXT_SOCAGE_KEY);
 
   if (storedContextId && !window.location.pathname.includes("context") ){
-  
     const params = new URLSearchParams()
     params.append("contextId", storedContextId)
-
     window.location.replace(`context-page/?${params.toString()}`)
-
   }
 
   if (contextProvider.currentContext?.id !== storedContextId) 
