e295f82d873d7bb9254206ba2525a9014753aa82
Kjetil Haugland <khaugl@equinor.com>
2023-01-16 14:20:34 +0100

    Init

---
+++ clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Equinor.ProjectExecutionPortal.ClientBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Equinor.ProjectExecutionPortal.ClientBackend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Equinor.ProjectExecutionPortal.ClientBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
