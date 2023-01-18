9bfa349085708fe98c74f2b3d85e49d2614b40fd
Gustav Eikaas <guei@equinor.com>
2023-01-18 14:12:00 +0100

    feat: :sparkles: improve appkey readability

--- client/packages/bookmarkManager/components/sectionList/SectionList.tsx
+++ client/packages/bookmarkManager/components/sectionList/SectionList.tsx
@@ -25,7 +25,10 @@ export function SectionList({ bookmarkGroups }: SectionList) {
       {bookmarkGroups
         .filter(filterEmptyGroups)
         .map(({ groupingProperty, values }) => (
-          <Section key={groupingProperty} name={groupingProperty}>
+          <Section
+            key={groupingProperty}
+            name={toHumanReadable(groupingProperty)}
+          >
             {values.sort(sortByName).map((b) => (
               <Row menuOptions={createMenuOptions(b)} key={b.id} name={b.name}>
                 {b.isShared && (
@@ -46,3 +49,9 @@ export function SectionList({ bookmarkGroups }: SectionList) {
 const filterEmptyGroups = (i: { values: Bookmark[] }) => i.values.length;
 const sortByName = (a: Bookmark, b: Bookmark) => a.name.localeCompare(b.name);
 const extractNameFromMail = (mail: string) => mail.split('@')[0].toLowerCase();
+
+const toHumanReadable = (val: string) =>
+  val
+    .split('-')
+    .map((s) => `${s[0].toUpperCase()}${s.slice(1)}`)
+    .join(' ');
