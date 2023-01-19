e96a0bdce995dafb7da2a04f91c6fc44c8efec33
Gustav Eikaas <guei@equinor.com>
2023-01-19 12:54:03 +0100

    feat: :sparkles: add snackbar message when sharing bookmark

--- client/packages/bookmarkManager/components/sectionList/SectionList.tsx
+++ client/packages/bookmarkManager/components/sectionList/SectionList.tsx
@@ -1,31 +1,42 @@
-import { Icon } from '@equinor/eds-core-react';
-import { useBookmarkGrouping, useDelete, useShare } from '../../hooks';
+import { Icon, Snackbar } from '@equinor/eds-core-react';
+import {
+  DeleteMutationArgs,
+  FavouriteMutationArgs,
+  ShareMutationArgs,
+  useBookmarkGrouping,
+  useDelete,
+  useFavourite,
+  useShare,
+} from '../../hooks';
 import { Bookmark } from '../../types';
 import { Row } from '../row/Row';
 import { Section } from '../section/Section';
 import { SharedIcon } from '../shared/SharedIcon';
+import { UseMutateFunction } from 'react-query';
+import { useState } from 'react';
 
 type SectionList = {
   bookmarkGroups: ReturnType<typeof useBookmarkGrouping>['bookmarkGroups'];
 };
 export function SectionList({ bookmarkGroups }: SectionList) {
   const { mutate: deleteMutation } = useDelete();
-  const { mutate: shareMutation } = useShare();
+  const { mutate: favouriteMutation } = useFavourite();
 
-  const createMenuOptions = ({ id, isShared }: Bookmark) => [
-    {
-      name: 'Remove',
-      disabled: false,
-      onClick: () => deleteMutation(id),
-      Icon: <Icon name="close" />,
-    },
-    {
-      name: isShared ? 'Unshare' : 'Share',
-      disabled: false,
-      onClick: () => shareMutation({ id, unShare: isShared }),
-      Icon: <Icon name="share" />,
-    },
-  ];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { mutate: shareMutation } = useShare(({ isShared, id }) => {
+    if (!isShared) return;
+    navigator.clipboard.writeText(appendBookmarkIdToUrl(id));
+    //Open snackbar
+    setIsOpen(true);
+  });
+
+  const createMenuOptions = (bm: Bookmark) =>
+    createBookmarkActions(bm, [
+      deleteMutation,
+      shareMutation,
+      favouriteMutation,
+    ]);
 
   return (
     <>
@@ -48,8 +59,50 @@ export function SectionList({ bookmarkGroups }: SectionList) {
             ))}
           </Section>
         ))}
+      <Snackbar
+        autoHideDuration={2000}
+        onClose={() => setIsOpen(false)}
+        open={isOpen}
+      >
+        Bookmark url copied to clipboard
+      </Snackbar>
     </>
   );
+}
+
+function createBookmarkActions(
+  { id, isShared, isMine }: Bookmark,
+  [deleteMutation, shareMutation, favoriteMutation]: [
+    UseMutateFunction<Response, unknown, DeleteMutationArgs, unknown>,
+    UseMutateFunction<Response, unknown, ShareMutationArgs, unknown>,
+    UseMutateFunction<Response, unknown, FavouriteMutationArgs, unknown>
+  ]
+) {
+  if (isMine) {
+    return [
+      {
+        name: 'Remove',
+        disabled: false,
+        onClick: () => deleteMutation({ id }),
+        Icon: <Icon name="close" />,
+      },
+      {
+        name: isShared ? 'Unshare' : 'Share',
+        disabled: false,
+        onClick: () => shareMutation({ id, unShare: isShared }),
+        Icon: <Icon name="share" />,
+      },
+    ];
+  } else {
+    return [
+      {
+        name: 'Unfavourite',
+        disabled: false,
+        onClick: () => favoriteMutation({ id, unFavourite: true }),
+        Icon: <Icon name="close" />,
+      },
+    ];
+  }
 }
 
 /** Helpers.. */
@@ -62,3 +115,10 @@ const toHumanReadable = (val: string) =>
     .split('-')
     .map((s) => `${s[0].toUpperCase()}${s.slice(1)}`)
     .join(' ');
+
+/**Appends bookmarkId to current url */
+function appendBookmarkIdToUrl(id: string): string {
+  const url = new URL(window.location.toString());
+  url.searchParams.set('bookmarkId', id);
+  return url.toString();
+}
