836f30e6446030ef94642f413e0c4b55cd04abae
Christopher Berge Hove <chbh@equinor.com>
2023-03-15 09:36:50 +0100

    feat: 107 add fusion navigation module to portal (#109)

--- client/packages/portal-client/src/components/portal-router/PortalRouter.tsx
+++ client/packages/portal-client/src/components/portal-router/PortalRouter.tsx
@@ -1,42 +1,44 @@
+import { useFramework } from '@equinor/fusion-framework-react';
 import { AppLoader } from '@equinor/portal-core';
 import { ContextPage, ViewPage } from '@equinor/portal-pages';
 import { PortalMessagePage } from '@equinor/portal-ui';
+import { NavigationModule } from '@equinor/fusion-framework-module-navigation';
 import { useMemo } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import { RouteObject, RouterProvider } from 'react-router-dom';
 
 import { PortalFrame } from '../portal-frame/PortalFrame';
 
+const routes: RouteObject[] = [
+	{
+		path: '/',
+		element: <PortalFrame />,
+		errorElement: <PortalMessagePage title="Fail to setup portal" type="Error" />,
+		children: [
+			{
+				path: '/',
+				element: <ViewPage />,
+				errorElement: <PortalMessagePage title="Fail to load view page" type={'Error'} />,
+			},
+			{
+				path: '/context-page/*',
+				element: <ContextPage />,
+				errorElement: <PortalMessagePage title="Fail to load context page" type={'Error'} />,
+			},
+			{
+				path: `/apps/:appKey/*`,
+				element: <AppLoader />,
+				errorElement: <PortalMessagePage title="Fail to load application" type={'Error'} />,
+			},
+		],
+	},
+];
+
 export function PortalRouter() {
-  let router = useMemo(() => {
-    return createBrowserRouter([
-      {
-        path: '/',
-        element: <PortalFrame />,
-        errorElement: <PortalMessagePage title="Fail to setup portal" type='Error' />,
-        children: [
-          {
-            path: '/',
-            element: <ViewPage />,
-            errorElement: <PortalMessagePage title="Fail to load view page" type={"Error"} />,
-          },
-          {
-            path: '/context-page/*',
-            element: <ContextPage />,
-            errorElement: <PortalMessagePage title="Fail to load context page" type={"Error"} />,
-          },
-          {
-            path: `/apps/:appKey/*`,
-            element: <AppLoader />,
-            errorElement: <PortalMessagePage title="Fail to load application" type={"Error"} />,
-          },
-        ],
-      },
+	const { navigation } = useFramework<[NavigationModule]>().modules;
 
-    ]);
-  }, []);
+	let router = useMemo(() => navigation.createRouter(routes), []);
 
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default PortalRouter;
