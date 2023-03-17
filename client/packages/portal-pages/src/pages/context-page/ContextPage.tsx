bf2b148c4f98d958d1017fff61361de9662973cf
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-03-17 14:19:22 +0100

    fix: useObservable fix

--- client/packages/portal-pages/src/pages/context-page/ContextPage.tsx
+++ client/packages/portal-pages/src/pages/context-page/ContextPage.tsx
@@ -1,5 +1,4 @@
-
-import {  Typography } from '@equinor/eds-core-react';
+import { Typography } from '@equinor/eds-core-react';
 
 import { useFrameworkCurrentContext } from '@equinor/portal-core';
 import { WorkAssigned } from '@equinor/portal-ui';
@@ -13,61 +12,62 @@ import { ProjectDescription } from './components/project-cards/ProjectDescriptio
 import { ProjectDetails } from './components/project-cards/ProjectDetails';
 
 import {
-  StyledBackground,
-  StyledContextPageGrid,
-  StyledGridItem,
-  StyledHeaderSection,
-  StyledCard
+	StyledBackground,
+	StyledContextPageGrid,
+	StyledGridItem,
+	StyledHeaderSection,
+	StyledCard,
 } from './ContextPage.Styles';
 
 function getBackgroundURL(instCode: string) {
-  return `https://stiddata.equinor.com/public/${instCode}.jpg`
-
+	return `https://stiddata.equinor.com/public/${instCode}.jpg`;
 }
 
 interface ProjectMaster extends Record<string, unknown> {
-  facilities: string[];
-  projectCategory: string;
-  cvpid: string;
-  documentManagementId: string;
-  phase: string;
-  portfolioOrganizationalUnit: string;
+	facilities: string[];
+	projectCategory: string;
+	cvpid: string;
+	documentManagementId: string;
+	phase: string;
+	portfolioOrganizationalUnit: string;
 }
 
 export const ContextPage = () => {
-  const currentContext = useFrameworkCurrentContext<ProjectMaster>();
+	const currentContext = useFrameworkCurrentContext<ProjectMaster>();
 
-  return (
-    <StyledMain>
-      <StyledBackground />
-      {currentContext?.value.facilities && <StyledHeaderSection url={getBackgroundURL(currentContext?.value.facilities[0])}>
-        <StyledCard>
-          <Typography variant='h3'>
-            <b>
-            {currentContext?.title}
-            </b>
-            </Typography>
-            <Typography variant='h3'>
-              { currentContext?.value.projectCategory.replace(new RegExp("-|\_\/*")," ") }
-            </Typography>
-        </StyledCard>
-        </StyledHeaderSection>}
-      <StyledContextPageGrid>
-        <StyledGridItem span={3} heightSpan={3}>
-          <ProjectDetails/>
-          <ProjectDescription/>
-        </StyledGridItem>
+	if (!currentContext) return null;
 
-        <StyledGridItem span={5}>
-          <Handover/>
-        </StyledGridItem>
-        <StyledGridItem span={4} heightSpan={3} >
-          < WorkAssigned/>
-        </StyledGridItem>
-        <StyledGridItem span={5} >
-          <Milestones/>
-        </StyledGridItem  >
-      </StyledContextPageGrid>
-    </StyledMain>
-  );
+	return (
+		<StyledMain>
+			<StyledBackground />
+			{currentContext.value.facilities && (
+				<StyledHeaderSection url={getBackgroundURL(currentContext.value.facilities[0])}>
+					<StyledCard>
+						<Typography variant="h3">
+							<b>{currentContext?.title}</b>
+						</Typography>
+						<Typography variant="h3">
+							{currentContext.value.projectCategory.replace(new RegExp('-|_/*'), ' ')}
+						</Typography>
+					</StyledCard>
+				</StyledHeaderSection>
+			)}
+			<StyledContextPageGrid>
+				<StyledGridItem span={3} heightSpan={3}>
+					<ProjectDetails />
+					<ProjectDescription />
+				</StyledGridItem>
+
+				<StyledGridItem span={5}>
+					<Handover />
+				</StyledGridItem>
+				<StyledGridItem span={4} heightSpan={3}>
+					<WorkAssigned />
+				</StyledGridItem>
+				<StyledGridItem span={5}>
+					<Milestones />
+				</StyledGridItem>
+			</StyledContextPageGrid>
+		</StyledMain>
+	);
 };
