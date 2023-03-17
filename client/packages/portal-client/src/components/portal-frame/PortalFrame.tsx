83ffa8d8836bcfe5925141d5be88fe655d2c0627
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-03-17 08:45:32 +0100

    chore(Header): renaming

--- client/packages/portal-client/src/components/portal-frame/PortalFrame.tsx
+++ client/packages/portal-client/src/components/portal-frame/PortalFrame.tsx
@@ -1,32 +1,33 @@
 import { MenuProvider, ViewProvider } from '@equinor/portal-core';
 import { PortalSideSheet, StyleProvider } from '@equinor/portal-ui';
-import { NotificationService, ServiceMessageProvider } from "@equinor/service-message"
+import { NotificationService, ServiceMessageProvider } from '@equinor/service-message';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import Header from '../portal-header/Header';
+import MainHeader from '../portal-header/Header';
 import { MenuGroups } from '../portal-menu/PortalMenu';
 export const PortalFrame = () => {
-  return (
-  <StyleProvider>
-    <StyledWrapper>
-      <ServiceMessageProvider>
-        <NotificationService>
-          <ViewProvider>
-            <MenuProvider>
-              <Header />
-              <MenuGroups />
-              <Outlet />
-            </MenuProvider>
-            <PortalSideSheet />
-          </ViewProvider>
-        </NotificationService>
-      </ServiceMessageProvider>
-    </StyledWrapper>
-  </StyleProvider>);
+	return (
+		<StyleProvider>
+			<StyledWrapper>
+				<ServiceMessageProvider>
+					<NotificationService>
+						<ViewProvider>
+							<MenuProvider>
+								<MainHeader />
+								<MenuGroups />
+								<Outlet />
+							</MenuProvider>
+							<PortalSideSheet />
+						</ViewProvider>
+					</NotificationService>
+				</ServiceMessageProvider>
+			</StyledWrapper>
+		</StyleProvider>
+	);
 };
 
 const StyledWrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
 `;
