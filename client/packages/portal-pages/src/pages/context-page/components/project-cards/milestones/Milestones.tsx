c395bf5eff814e5242ddc809da920cb338f81b78
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-03-21 12:16:48 +0100

    fix: style updates

--- client/packages/portal-pages/src/pages/context-page/components/project-cards/milestones/Milestones.tsx
+++ client/packages/portal-pages/src/pages/context-page/components/project-cards/milestones/Milestones.tsx
@@ -1,45 +1,79 @@
-import {  Button, EdsProvider, Icon, Slider, Table, Typography } from '@equinor/eds-core-react';
+import { Button, EdsProvider, Icon, Slider, Table, Typography } from '@equinor/eds-core-react';
 import Skeleton, { SkeletonVariant } from '@equinor/fusion-react-skeleton';
 import { DateTime } from 'luxon';
-
 
 import { StyledCardWrapper, StyledContent, StyledHeader } from '../styles';
 import { useMilestoneQuery } from './use-presence-query';
 
-function verifyDate (date: string): string {
-    return new Date(date).toString() !== "Invalid Date" ? DateTime.fromJSDate(new Date(date)).toFormat("dd LLL yyyy"): "-";
-} 
+function verifyDate(date: string): string {
+	return new Date(date).toString() !== 'Invalid Date'
+		? DateTime.fromJSDate(new Date(date)).toFormat('dd LLL yyyy')
+		: '-';
+}
 
-const LoadingSkeleton = ()=> <>
-        <Table.Row  >
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-       </Table.Row>
-        <Table.Row  >
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-       </Table.Row>
-        <Table.Row  >
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-       </Table.Row>
-        <Table.Row  >
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-            <Table.Cell><Skeleton variant={SkeletonVariant.Text}/></Table.Cell>
-       </Table.Row>
-</>
-
+const LoadingSkeleton = () => (
+	<>
+		<Table.Row>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+		</Table.Row>
+		<Table.Row>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+		</Table.Row>
+		<Table.Row>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+		</Table.Row>
+		<Table.Row>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+			<Table.Cell>
+				<Skeleton variant={SkeletonVariant.Text} />
+			</Table.Cell>
+		</Table.Row>
+	</>
+);
 
 export const Milestones = () => {
-    const {data} = useMilestoneQuery() 
+	const { data } = useMilestoneQuery();
 
 	return (
 		<StyledCardWrapper>
@@ -47,40 +81,37 @@ export const Milestones = () => {
 				<Typography variant="h5">Milestones</Typography>
 			</StyledHeader>
 			<StyledContent>
-
-            <EdsProvider density='compact'>
-            <Table>
-                <Table.Head>
-                    <Table.Row>
-                        <Table.Cell>Milestone</Table.Cell>
-                        <Table.Cell>Description</Table.Cell>
-                        <Table.Cell>Planned</Table.Cell>
-                        <Table.Cell>Forecast</Table.Cell>
-              
-                    </Table.Row>
-                </Table.Head>
-                <Table.Body>
-                    {data ? data.map((milestone) => { 
-                        const datePlanned = verifyDate(milestone.datePlanned); 
-                        const dateForecast = verifyDate(milestone.dateForecast);
-                        return (  <Table.Row key={milestone.milestone} >
-                        <Table.Cell>{milestone.milestone}</Table.Cell>
-                        <Table.Cell>{milestone.description}</Table.Cell>
-                        <Table.Cell>{datePlanned}</Table.Cell>
-                        <Table.Cell>{dateForecast}</Table.Cell>
-                    </Table.Row>) 
-                    }) : <LoadingSkeleton/>
-                    } 
-                </Table.Body>
-            </Table>
-            </EdsProvider>
+				<EdsProvider density="compact">
+					<Table>
+						<Table.Head>
+							<Table.Row>
+								<Table.Cell>Milestone</Table.Cell>
+								<Table.Cell>Description</Table.Cell>
+								<Table.Cell>Planned</Table.Cell>
+								<Table.Cell>Forecast</Table.Cell>
+							</Table.Row>
+						</Table.Head>
+						<Table.Body>
+							{data ? (
+								data.map((milestone) => {
+									const datePlanned = verifyDate(milestone.datePlanned);
+									const dateForecast = verifyDate(milestone.dateForecast);
+									return (
+										<Table.Row key={milestone.milestone}>
+											<Table.Cell>{milestone.milestone}</Table.Cell>
+											<Table.Cell>{milestone.description}</Table.Cell>
+											<Table.Cell>{datePlanned}</Table.Cell>
+											<Table.Cell>{dateForecast}</Table.Cell>
+										</Table.Row>
+									);
+								})
+							) : (
+								<LoadingSkeleton />
+							)}
+						</Table.Body>
+					</Table>
+				</EdsProvider>
 			</StyledContent>
-            <StyledCardWrapper.Actions alignRight={true}>
-                <Button variant='ghost' disabled={true}>
-                    Go to app
-                    <Icon name="chevron_right"/>
-                </Button>
-            </StyledCardWrapper.Actions>
 		</StyledCardWrapper>
 	);
 };
--- client/packages/portal-pages/src/pages/context-page/components/project-cards/styles.ts
+++ client/packages/portal-pages/src/pages/context-page/components/project-cards/styles.ts
@@ -1,31 +1,29 @@
-import { Card } from "@equinor/eds-core-react";
-import styled from "styled-components";
+import { Card } from '@equinor/eds-core-react';
+import styled from 'styled-components';
 
-export const StyledCardWrapper = styled(Card)<{color?: string}>`
-    display: flex;
-    flex-direction: column;
-    background-color: ${({color})=> color};
-    overflow: hidden;
+export const StyledCardWrapper = styled(Card)<{ color?: string }>`
+	display: flex;
+	flex-direction: column;
+	background-color: ${({ color }) => color};
+	overflow: hidden;
 `;
 
 export const StyledHeader = styled(Card.Header)`
-    display: flex;
+	display: flex;
 `;
 
 export const StyledContent = styled(Card.Content)`
-    display: flex;
-    flex-direction: column;
-    padding-top: 1rem;
-    gap: 1rem;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
 `;
 
 export const StyledContentRow = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    padding-bottom: 1rem;
-
+	display: flex;
+	justify-content: flex-end;
+	padding-bottom: 1rem;
 `;
 
 export const StyledContentItem = styled.div`
-    flex: 1;
+	flex: 1;
 `;
