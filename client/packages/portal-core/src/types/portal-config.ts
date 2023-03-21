0d446a2c79039f855eda74e6fd904b274694547a
Christopher Berge Hove <chbh@equinor.com>
2023-03-21 10:06:10 +0100

    fix: allow for backend to provide config at startup by adding config … (#122)

--- client/packages/portal-core/src/types/portal-config.ts
+++ client/packages/portal-core/src/types/portal-config.ts
@@ -4,39 +4,41 @@ import { View } from './view';
 export type LoggerLevel = 0 | 1 | 2 | 4 | 3;
 
 export interface PortalConfig {
-  serviceDiscovery: {
-    client: Client;
-  };
-  portalClient: {
-    client: Client;
-  };
-  masal: {
-    client: AuthClientOptions;
-    options: {
-      requiresAuth: boolean;
-      clients?: Record<string, AuthClientOptions>;
-    };
-  };
-  logger?: {
-    level: number;
-    defaultClientLogger?: {
-      active: boolean;
-      level: number;
-    };
-  };
-  applications?: {
-    client: Client;
-  };
-  appConfig?: {
-    client: Client;
-  };
-  agGrid?: {
-    licenseKey: string;
-  };
-  views?: View[];
+	title: string;
+	description: string;
+	serviceDiscovery: {
+		client: Client;
+	};
+	portalClient: {
+		client: Client;
+	};
+	masal: {
+		client: AuthClientOptions;
+		options: {
+			requiresAuth: boolean;
+			clients?: Record<string, AuthClientOptions>;
+		};
+	};
+	logger?: {
+		level: number;
+		defaultClientLogger?: {
+			active: boolean;
+			level: number;
+		};
+	};
+	applications?: {
+		client: Client;
+	};
+	appConfig?: {
+		client: Client;
+	};
+	agGrid?: {
+		licenseKey: string;
+	};
+	views?: View[];
 }
 
 export interface Client {
-  baseUri: string;
-  defaultScopes: string[];
+	baseUri: string;
+	defaultScopes: string[];
 }
