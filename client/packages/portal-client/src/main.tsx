3e3ed24b25ac24133df8c6ac69580b9778015ccc
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-03-21 08:10:42 +0100

    fix: allow for backend to provide config at startup by adding config to window

--- client/packages/portal-client/src/main.tsx
+++ client/packages/portal-client/src/main.tsx
@@ -1,31 +1,34 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from 'react-query';
-import * as portalConfig from './portal.config.json';
+// import * as portalConfig from './portal.config.json';
 import Framework from '@equinor/fusion-framework-react';
 
 import { PortalProgressLoader } from '@equinor/portal-ui';
 import PortalRouter from './components/portal-router/PortalRouter';
 import { queryClient } from './utils/queryClient/query-client';
 import { createPortalFramework } from './framework-config';
+import { PortalConfig } from '../../portal-core/src';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const portalConfig = window['_config_'];
 
 document.title = `${portalConfig.title} | Fusion`;
-
-const configure = createPortalFramework(portalConfig.config);
+const configure = createPortalFramework(portalConfig);
 
 root.render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Framework
-        configure={configure}
-        fallback={<PortalProgressLoader title="Configuring Portal" />}
-      >
-        <PortalRouter />
-      </Framework>
-    </QueryClientProvider>
-  </StrictMode>
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<Framework configure={configure} fallback={<PortalProgressLoader title="Configuring Portal" />}>
+				<PortalRouter />
+			</Framework>
+		</QueryClientProvider>
+	</StrictMode>
 );
+
+declare global {
+	interface Window {
+		_config_: PortalConfig;
+	}
+}
