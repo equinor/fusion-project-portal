a3517dcaa817f96933d1516f8f80d5b7865325ef
Kjetil Haugland <khaugl@equinor.com>
2023-03-29 14:32:14 +0200

    .

--- clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Controllers/MainController.cs
+++ clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Controllers/MainController.cs
@@ -1,10 +1,5 @@
-﻿using System.Diagnostics;
-using Equinor.ProjectExecutionPortal.ClientBackend.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Web;
 
 namespace Equinor.ProjectExecutionPortal.ClientBackend.Controllers
 {
@@ -12,35 +7,16 @@ namespace Equinor.ProjectExecutionPortal.ClientBackend.Controllers
     {
         private readonly ILogger<MainController> _logger;
 
-        private const string AuthSchemes = OpenIdConnectDefaults.AuthenticationScheme + "," +
-                                           JwtBearerDefaults.AuthenticationScheme;
-
         public MainController(ILogger<MainController> logger)
         {
             _logger = logger;
         }
 
-        [AllowAnonymous]
-        //[AuthorizeForScopes(Scopes = new string[] { })]
-        public async Task<IActionResult> Index([FromServices] ITokenAcquisition tokenProvider)
+        //[AllowAnonymous]
+        [Authorize]
+        public Task<IActionResult> Index()
         {
-            //var scopes = new string[] { };
-            //var accessToken = await tokenProvider.GetAccessTokenForUserAsync(scopes);
-
-            //ViewData["accessToken"] = accessToken;
-
-            return View();
-        }
-
-        //public IActionResult Index()
-        //{
-        //    return View("./test.html");
-        //}
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Task.FromResult<IActionResult>(View());
         }
     }
 }
--- clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Program.cs
+++ clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Program.cs
@@ -1,13 +1,9 @@
-﻿using System.Text.Json.Serialization;
-using Equinor.ProjectExecutionPortal.ClientBackend;
+﻿using Equinor.ProjectExecutionPortal.ClientBackend;
 using Equinor.ProjectExecutionPortal.ClientBackend.AssetProxy;
 using Fusion.Integration;
 using Fusion.Integration.Http;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Client;
 using Microsoft.Identity.Web;
 
@@ -17,22 +13,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<CacheOptions>(builder.Configuration.GetSection("CacheOptions"));
 builder.Services.Configure<AssetProxyOptions>(builder.Configuration.GetSection("AssetProxy"));
 
-// TODO: https://weblog.west-wind.com/posts/2022/Mar/29/Combining-Bearer-Token-and-Cookie-Auth-in-ASPNET
-// https://medium.com/@niteshsinghal85/how-to-support-multiple-authentication-scheme-in-asp-net-core-api-581e47e38de3
-// https://stackoverflow.com/questions/68081122/getting-access-token-to-call-an-api-from-my-asp-net-core-5-mvc-web-app
-
 // Add cookie auth
 builder.Services
     .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration)
-    .EnableTokenAcquisitionToCallDownstreamApi(new string[] {  })
+    .EnableTokenAcquisitionToCallDownstreamApi(new string[] { })
     .AddInMemoryTokenCaches();
 
 // Add bearer auth
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration)
-    .EnableTokenAcquisitionToCallDownstreamApi()
-    .AddInMemoryTokenCaches();
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration)
+//    .EnableTokenAcquisitionToCallDownstreamApi()
+//    .AddInMemoryTokenCaches();
 
 // Authorization
 builder.Services.AddAuthorization(options =>
@@ -82,7 +74,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Main/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -92,11 +83,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseAuthentication();
-app.UseAuthorization();
 
 app.UseRouting();
 
-
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
