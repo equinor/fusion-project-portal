257c636d5800be1353402a34eab739f09e9dac82
Kjetil Haugland <khaugl@equinor.com>
2023-04-12 10:30:25 +0200

    Add migration for removing contextBase

---
+++ backend/src/Equinor.ProjectExecutionPortal.Infrastructure/Migrations/20230412082841_RemoveContextBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProjectExecutionPortal.Infrastructure.Migrations
+{
+    public partial class RemoveContextBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "WorkSurfaceApps");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "WorkSurfaceApps");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "WorkSurfaceApps",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "WorkSurfaceApps",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
