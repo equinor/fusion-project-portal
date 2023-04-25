c2480c45fccf4f0aea320658bfdb5492b3baeb11
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-04-25 14:39:49 +0200

    fix: navigate on context select in header if context is not present in url

--- client/packages/portal-ui/src/lib/header/HeaderContextSelector.tsx
+++ client/packages/portal-ui/src/lib/header/HeaderContextSelector.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { ContextSelector, useViewController } from '@equinor/portal-core';
+import { useNavigate, useParams } from 'react-router-dom';
 export { ContextProvider } from '@equinor/portal-core';
 
 const StyledWrapper = styled.span`
@@ -9,10 +10,16 @@ const StyledWrapper = styled.span`
 
 export const TopBarContextSelector = () => {
 	const { currentView } = useViewController();
+	const { contextId } = useParams();
+	const navigate = useNavigate();
 	if (!currentView) return null;
 	return (
 		<StyledWrapper>
-			<ContextSelector path={`/${currentView.key}`} variant={'header'} />
+			<ContextSelector
+				path={`/${currentView.key}`}
+				variant={'header'}
+				navigate={!contextId ? navigate : undefined}
+			/>
 		</StyledWrapper>
 	);
 };
