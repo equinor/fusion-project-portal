a445fa15642d69ae8f83c2c40e8527bfd7933bdd
Kjetil Haugland <801342+kjetilhau@users.noreply.github.com>
2023-04-28 09:41:42 +0200

    feat: Add support for AgGrid license key in appSettings and Radix (prod only) (#173)

--- clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Configurations/ClientBundleOptions.cs
+++ clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Configurations/ClientBundleOptions.cs
@@ -5,6 +5,7 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public ClientBundleMsalOptions? Msal { get; set; }
+        public ClientBundleAgGridOptions? AgGrid { get; set; }
     }
 
     public class ClientBundleMsalOptions
@@ -12,5 +13,10 @@
         public string? TenantId { get; set; }
         public string? ClientId { get; set; }
         public string? RedirectUri { get; set; }
+    }
+
+    public class ClientBundleAgGridOptions
+    {
+        public string? LicenseKey { get; set; }
     }
 }
