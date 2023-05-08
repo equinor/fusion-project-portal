615fe204e30f46d5d49a1bd236e06264fa1a3dc8
Kjetil Haugland <801342+kjetilhau@users.noreply.github.com>
2023-05-08 14:12:47 +0200

    feat: Added Fusion Bookmarks env config variables (#179)

---
+++ clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Configurations/FusionBookmarksOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Equinor.ProjectExecutionPortal.ClientBackend.Configurations
+{
+    public class FusionBookmarksOptions
+    {
+        public string? Name { get; set; }
+        public string? Identifier { get; set; } 
+        public string? Subsystem { get; set; }
+    }
+}
