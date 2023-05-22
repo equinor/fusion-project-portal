affadb5638f6d00104c9040be4e5e4f06f8d935d
espenkalle <eskal@equinor.com>
2023-05-22 14:45:34 +0200

    fix: :bug: duplicate search result when pinned

--- client/packages/portal-core/src/app/appsMatchingSearch.ts
+++ client/packages/portal-core/src/app/appsMatchingSearch.ts
@@ -1,8 +1,10 @@
 import { AppGroup } from '../types';
+import { AppManifest } from '@equinor/fusion-framework-app';
 
-export function appsMatchingSearch(groups: AppGroup[], searchText?: string, clickedCategoryItems?: any[]) {
+export function appsMatchingSearch(groups: AppGroup[], searchText?: string, clickedCategoryItems?: any[], favorites?: AppManifest[]) {
 	if (!searchText) return groups;
 
+	let favoritesAppKeys = favorites.map((obj) => obj.key);
 	return groups
 		.map((group) => ({
 			...group,
@@ -11,8 +13,9 @@ export function appsMatchingSearch(groups: AppGroup[], searchText?: string, clic
 					!clickedCategoryItems || !clickedCategoryItems.includes(group.name)
 			).filter(
 				(app) =>
-					app.appKey.toLowerCase().includes(searchText.toLowerCase()) ||
-					app.name.toLowerCase().includes(searchText.toLowerCase())
+					(app.appKey.toLowerCase().includes(searchText.toLowerCase()) ||
+					app.name.toLowerCase().includes(searchText.toLowerCase())) && 
+					!favoritesAppKeys.includes(app.appKey.toLowerCase())
 			),
 		}))
 		.filter((group) => group.apps.length);
