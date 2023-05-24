e050b6c69594fb7ec040ed0e0ff2fe5aeeb68b9e
Kjetil Haugland <khaugl@equinor.com>
2023-05-24 12:20:25 +0200

    Revert

--- backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/BundleController.cs
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/BundleController.cs
@@ -1,16 +1,16 @@
 ﻿using Equinor.ProjectExecutionPortal.FusionPortalApi.Apps;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Equinor.ProjectExecutionPortal.WebApi.Controllers
 {
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [ApiVersion("0.1")]
     [Route("api/bundles")]
     [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
     public class BundleController : ApiControllerBase
     {
+        [AllowAnonymous]
         [HttpGet("{appKey}")]
         [HttpGet("{appKey}.js")]
         public async Task<ActionResult> FusionPortalAppBundle([FromServices] IFusionPortalApiService fusionPortalApiService, [FromRoute] string appKey)
