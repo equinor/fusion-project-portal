b5755aaa1ee76d021f7727a04bf3631bc6c451ee
Kjetil Haugland <801342+kjetilhau@users.noreply.github.com>
2023-05-24 14:16:14 +0200

    feat: disable caching on bundles endpoint (#186)

--- backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/BundleController.cs
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/BundleController.cs
@@ -7,6 +7,7 @@ namespace Equinor.ProjectExecutionPortal.WebApi.Controllers
     //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [ApiVersion("0.1")]
     [Route("api/bundles")]
+    [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
     public class BundleController : ApiControllerBase
     {
         [AllowAnonymous]
