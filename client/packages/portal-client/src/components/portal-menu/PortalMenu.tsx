c4cee254aa1f4bb4781a1542d2d5f8a38d2ce12e
espenkalle <eskal@equinor.com>
2023-05-24 14:59:32 +0200

    feat: :sparkles: dynamic menu based on appgroup amount

--- client/packages/portal-client/src/components/portal-menu/PortalMenu.tsx
+++ client/packages/portal-client/src/components/portal-menu/PortalMenu.tsx
@@ -8,35 +8,6 @@ import { combineLatest, map } from 'rxjs';
 import styled from 'styled-components';
 import { InfoMessage } from 'packages/portal-ui/src/lib/info-message/InfoMessage';
 
-const AppsWrapper = styled.div`
-	padding: 1rem 0 1rem 1rem;
-	height: 350px;
-	display: block;
-	grid-template-columns: auto;
-	padding-bottom: 2rem;
-	column-width: auto;
-	column-count: 2;
-`;
-
-const MenyWrapper = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: flex-start;
-	padding: 1rem 0 1rem 0;
-	gap: 3rem;
-	height: 90%;
-`;
-
-const CategoryWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	padding: 1rem 1rem 1rem 0;
-	white-space: nowrap;
-	border-right: 1px solid #dcdcdc;
-	height: 100%;
-	width: 300px;
-`;
-
 export function MenuGroups() {
 	const { data, isLoading } = useAppGroupsQuery();
 	const [searchText, setSearchText] = useState<string | undefined>('');
@@ -52,41 +23,32 @@ export function MenuGroups() {
 			)
 		) ?? [];
 
-	const favoriteAppKeys = favorites.map((favorites) => favorites.key);
-	const enabledApps = (data?.map((group) => group.apps) ?? []).flat();
-	const favortiedApps = enabledApps
-		.filter((app) => favoriteAppKeys.includes(app.appKey))
-		.map(
-			(app): App => ({
-				appKey: app.appKey,
-				description: app.description ?? '',
-				name: app.name,
-				isDisabled: false,
-				order: app.order ?? 0,
-			})
-		);
+	const categoryItems = ['Pinned Apps', ...(data?.map((item) => item.name) ?? []), 'All Apps'];
 
-	const allAppKeys = enabledApps.map((app) => app.appKey);
-	const disabledApps = favorites
-		.filter((favorite) => !allAppKeys.includes(favorite.key))
-		.map(
-			(disabledApp): App => ({
-				appKey: disabledApp.key,
-				description: disabledApp.description ?? '',
-				name: disabledApp.name,
-				isDisabled: true,
-				order: disabledApp.order ?? 0,
-			})
-		);
+	const getColumnCount = () => {
+		if (!data) {
+			return 1;
+		}
 
-	const favoriteGroup: AppGroup = {
-		name: 'Pinned Apps',
-		order: 0,
-		accentColor: '#ece90f',
-		apps: [...favortiedApps, ...disabledApps],
+		if (data?.length > 3 && data?.length < 10) {
+			return 2;
+		} else if (data?.length < 3) {
+			return 1;
+		}
+		return 3;
 	};
 
-	const categoryItems = ['Pinned Apps', ...(data?.map((item) => item.name) ?? []), 'All Apps'];
+	const getMenuWidth = () => {
+		if (!data) {
+			return 750;
+		}
+		if (data?.length > 3 && data?.length < 10) {
+			return 1100;
+		} else if (data.length < 3) {
+			return 750;
+		}
+		return 1450;
+	};
 
 	const customSort = (a: AppGroup, b: AppGroup) => {
 		if (a.name === activeItem) {
@@ -104,18 +66,54 @@ export function MenuGroups() {
 		}
 	};
 
-	const memoizedResult = useMemo(() => {
+
+	const favoriteGroup = useMemo(() => {
+		const favoriteAppKeys = favorites.map((favorites) => favorites.key);
+		const enabledApps = (data?.map((group) => group.apps) ?? []).flat();
+		const favortiedApps = enabledApps
+			.filter((app) => favoriteAppKeys.includes(app.appKey))
+			.map(
+				(app): App => ({
+					appKey: app.appKey,
+					description: app.description ?? '',
+					name: app.name,
+					isDisabled: false,
+					order: app.order ?? 0,
+				})
+			);
+
+		const allAppKeys = enabledApps.map((app) => app.appKey);
+		const disabledApps = favorites
+			.filter((favorite) => !allAppKeys.includes(favorite.key))
+			.map(
+				(disabledApp): App => ({
+					appKey: disabledApp.key,
+					description: disabledApp.description ?? '',
+					name: disabledApp.name,
+					isDisabled: true,
+					order: disabledApp.order ?? 0,
+				})
+			);
+
+		return {
+			name: 'Pinned Apps',
+			order: 0,
+			accentColor: '#ece90f',
+			apps: [...favortiedApps, ...disabledApps],
+		};
+	}, [favorites, data, []]);
+
+	const displayAppGroups = useMemo(() => {
 		if (activeItem.includes('Pinned Apps') && searchText === '') {
 			return [favoriteGroup];
 		}
 		if (searchText != '' || activeItem.includes('All Apps')) {
 			const appSearch = appsMatchingSearch(data ?? [], searchText);
-			console.log(appSearch);
 			return appSearch.sort(customSort);
 		}
 		const filteredApps = data?.filter((obj) => obj.name === activeItem);
 		return filteredApps;
-	}, [searchText, activeItem]);
+	}, [searchText, activeItem, []]);
 
 	const handleToggle = (name: string) => {
 		if (activeItem === name) {
@@ -125,8 +123,37 @@ export function MenuGroups() {
 		}
 	};
 
+	const AppsWrapper = styled.div`
+		padding: 1rem 0 1rem 1rem;
+		height: 350px;
+		display: block;
+		grid-template-columns: auto;
+		padding-bottom: 2rem;
+		column-width: auto;
+		column-count: ${getColumnCount()};
+	`;
+
+	const MenyWrapper = styled.div`
+		display: flex;
+		flex-direction: row;
+		align-items: flex-start;
+		padding: 1rem 0 1rem 0;
+		gap: 3rem;
+		height: 90%;
+	`;
+
+	const CategoryWrapper = styled.div`
+		display: flex;
+		flex-direction: column;
+		padding: 1rem 1rem 1rem 0;
+		white-space: nowrap;
+		border-right: 1px solid #dcdcdc;
+		height: 100%;
+		width: 300px;
+	`;
+
 	return (
-		<PortalMenu>
+		<PortalMenu width={getMenuWidth()}>
 			<Search
 				id="app-search"
 				placeholder="Search for apps"
@@ -149,19 +176,19 @@ export function MenuGroups() {
 								/>
 							))}
 						</CategoryWrapper>
-						{memoizedResult?.length > 0 ? (
-							<AppsWrapper>
-								{activeItem.includes('Pinned Apps') && favorites?.length === 0 ? (
-									<InfoMessage>
-										Looks like you do not have any pinned apps yet. Click the star icon on apps to
-										add them to the pinned app section.
-									</InfoMessage>
-								) : null}
-
-								{<GroupWrapper appGroups={memoizedResult} />}
-							</AppsWrapper>
+						{displayAppGroups?.length > 0 ? (
+							activeItem.includes('Pinned Apps') && favorites?.length === 0 ? (
+								<InfoMessage>
+									Looks like you do not have any pinned apps yet. Click the star icon on apps to add
+									them to the pinned app section.
+								</InfoMessage>
+							) : (
+								<AppsWrapper>
+									<GroupWrapper appGroups={displayAppGroups} />
+								</AppsWrapper>
+							)
 						) : (
-							<InfoMessage>No results found for your search.</InfoMessage>
+							<>{searchText ? <InfoMessage>No results found for your search.</InfoMessage> : null}</>
 						)}
 					</>
 				)}
--- client/packages/portal-ui/src/lib/portal-menu/PortalMenu.tsx
+++ client/packages/portal-ui/src/lib/portal-menu/PortalMenu.tsx
@@ -3,13 +3,17 @@ import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 import { MenuScrim, MenuWrapper } from './MenuStyles';
 
-export const PortalMenu = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
-	const { menuActive, closeMenu } = useMenuContext();
-	return (
-		<MenuScrim open={menuActive} isDismissable onClose={closeMenu}>
-			<MenuWrapper style={{ width: '1100px' }}>{children}</MenuWrapper>
-		</MenuScrim>
-	);
+interface PortalMenuProps {
+  width: number;
+}
+
+export const PortalMenu = ({ width, children }: PropsWithChildren<PortalMenuProps>): JSX.Element => {
+  const { menuActive, closeMenu } = useMenuContext();
+  return (
+    <MenuScrim open={menuActive} isDismissable onClose={closeMenu}>
+      <MenuWrapper style={{ width: `${width}px` }}>{children}</MenuWrapper>
+    </MenuScrim>
+  );
 };
 
 export default PortalMenu;
