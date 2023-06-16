c5949459d0dd49d8b3233bf592c8d90d4944663d
Kjetil Haugland <khaugl@equinor.com>
2023-06-16 11:00:53 +0200

    Remove dash in search pattern

--- clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Services/ClientBundleService.cs
+++ clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Services/ClientBundleService.cs
@@ -3,7 +3,7 @@
     public class ClientBundleService : IClientBundleService
     {
         private readonly IWebHostEnvironment _environment;
-        private const string PortalClientFileName = "index-*";
+        private const string PortalClientFileName = "index*";
 
         public ClientBundleService(IWebHostEnvironment environment)
         {
