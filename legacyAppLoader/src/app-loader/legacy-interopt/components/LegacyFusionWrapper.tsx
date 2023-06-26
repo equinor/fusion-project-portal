7a7a2391d432ee1f8d0bb6b33d56d37ee081a82f
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-06-26 12:19:52 +0200

    fix: fixing fusion root style issues

--- legacyAppLoader/src/app-loader/legacy-interopt/components/LegacyFusionWrapper.tsx
+++ legacyAppLoader/src/app-loader/legacy-interopt/components/LegacyFusionWrapper.tsx
@@ -1,46 +1,34 @@
 /* eslint-disable react/no-multi-comp */
-import {
-  PropsWithChildren,
-  ReactChild,
-  ReactElement,
-  ReactNode,
-  Suspense,
-  useMemo,
-  useRef,
-} from "react";
+import { PropsWithChildren, ReactNode, Suspense, useMemo, useRef } from "react";
+import { FusionRoot } from "@equinor/fusion-components";
 
 import { createLegacyContextComponent } from "./create-legacy-context-component";
 import type { PortalFramework } from "../types";
 import { FusionContextOptions } from "../create-fusion-context";
 import { AppModulesInstance } from "@equinor/fusion-framework-react-app";
 import { AppModule } from "@equinor/fusion-framework-module-app";
+import { styled } from "styled-components";
 
 export type LegacyFusionWrapperProps = {
   loader: NonNullable<ReactNode>;
   framework: PortalFramework;
   options?: FusionContextOptions;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  RootWrapper?: (props: { children: ReactChild }) => ReactElement<any, any>;
   appFramework: AppModulesInstance<[AppModule]>;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const FallThrewComponent = ({
-  children,
-}: {
-  children: ReactChild;
-}): ReactElement<any, any> => <>{children}</>;
+// Fixing Fusion root style issues
+const StyleFix = styled.span`
+  width: 100%;
+  > div > div {
+    display: block !important;
+  }
+`;
 
 export const LegacyFusionWrapper = (
   props: PropsWithChildren<LegacyFusionWrapperProps>
 ) => {
-  const {
-    framework,
-    options,
-    loader,
-    RootWrapper = FallThrewComponent,
-    appFramework,
-  } = props;
+  const { framework, options, loader, appFramework } = props;
   const root = useRef(null);
   const overlay = useRef(null);
   const headerContent = useRef<HTMLElement | null>(null);
@@ -58,11 +46,11 @@ export const LegacyFusionWrapper = (
   return (
     <Suspense fallback={loader}>
       <LegacyContext>
-        <RootWrapper>
-          {/* <FusionRoot rootRef={root} overlayRef={overlay}> */}
-          <>{props.children}</>
-          {/* </FusionRoot> */}
-        </RootWrapper>
+        <StyleFix>
+          <FusionRoot rootRef={root} overlayRef={overlay} noHeader={true}>
+            {props.children}
+          </FusionRoot>
+        </StyleFix>
       </LegacyContext>
     </Suspense>
   );
