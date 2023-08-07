02cf9ec6e60374e2df833a9d3cd45c7abfa8e744
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-08-07 12:34:05 +0200

    chore: config cleanup

--- client/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import { getJestProjects } from '@nrwl/jest';
-import type { Config } from 'jest';
-
-const config: Config = {
-  projects: getJestProjects(),
-  verbose: true,
-};
-
-export default config;
--- client/packages/portal-ui/src/header/Header.spec.tsx
+++ client/packages/portal-ui/src/header/Header.spec.tsx
@@ -2,10 +2,18 @@ import { Button } from '@equinor/eds-core-react';
 import { render } from '@testing-library/react';
 
 import Header from './Header';
+import React from 'react';
 
 describe('PortalTopBar', () => {
 	it('should render successfully', () => {
-		const { baseElement } = render(<Header MenuButton={() => <Button>Button</Button>} title="test" />);
+		const { baseElement } = render(
+			<Header
+				MenuButton={() => <Button>Button</Button>}
+				onLogoClick={() => {
+					//
+				}}
+			/>
+		);
 		expect(baseElement).toBeTruthy();
 	});
 });
