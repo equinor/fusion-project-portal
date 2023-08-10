534e327ccdd9baeb8623bf2d31145978834e45ac
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-08-10 14:18:20 +0200

    chore: move resource loader to its own project

---
+++ resourceLoader/src/globalResources.ts
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as fusion from '@equinor/fusion';
+import * as fusionComponents from '@equinor/fusion-components';
+import * as fusionReactStyles from '@equinor/fusion-react-styles';
+import * as FusionReactRouter from 'react-router-dom';
+
+declare global {
+	interface Window {
+		FusionReact: typeof React;
+		FusionReactDOM: typeof ReactDOM;
+		FusionReactRouter: typeof FusionReactRouter;
+		FusionAPI: typeof fusion;
+		FusionComponents: typeof fusionComponents;
+		FusionReactStyles: typeof fusionReactStyles;
+		clientBaseUri: string;
+	}
+}
+export function addGlobalDependencies() {
+	window['FusionReactDOM'] = ReactDOM;
+	window['FusionReact'] = React;
+	window['FusionReactRouter'] = FusionReactRouter;
+	window['FusionAPI'] = fusion;
+	window['FusionComponents'] = fusionComponents;
+	window['FusionReactStyles'] = fusionReactStyles;
+}
+
+addGlobalDependencies();
