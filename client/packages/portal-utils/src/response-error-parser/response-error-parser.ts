174be507640f3b7b6add187557c76a58b4e6f302
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-08-14 09:15:11 +0200

    chore: added response error tests

--- client/packages/portal-utils/src/response-error-parser/response-error-parser.ts
+++ client/packages/portal-utils/src/response-error-parser/response-error-parser.ts
@@ -1,18 +1,21 @@
 export const responseErrorParser = (res: Response) => {
-  switch (res.status) {
-    case 500: {
-      return 'Server failed to respond';
-    }
-    case 401: {
-      return 'Failed to authenticate, reload the page and try again';
-    }
+	switch (res.status) {
+		case 500: {
+			return 'Server failed to respond';
+		}
+		case 401: {
+			return 'Failed to authenticate, reload the page and try again';
+		}
 
-    case 403: {
-      return `It looks like you don't have permission to view this page`;
-    }
+		case 403: {
+			return `It looks like you don't have permission to view this page`;
+		}
+		case 404: {
+			return `Resource not found.`;
+		}
 
-    default: {
-      return 'We are not sure whats wrong';
-    }
-  }
+		default: {
+			return 'We are not sure whats wrong';
+		}
+	}
 };
