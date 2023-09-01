569662c5cf2cf37bcbfc3741f3aefa180ef1a902
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-09-01 12:44:00 +0200

    chore: moved FusionLegacyEnvIdentifier to ClientBundleOptions and removed FusionLegacyPortalOptions

--- clientBackend/src/Equinor.ProjectExecutionPortal.ClientBackend/Configurations/FusionLegacyPortalOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Equinor.ProjectExecutionPortal.ClientBackend.Configurations
-{
-    public class FusionLegacyPortalOptions
-    {
-        public string? FusionLegacyEnvIdentifier { get; set; }
-
-    }
-}
