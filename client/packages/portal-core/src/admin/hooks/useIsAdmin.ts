682de95c58f79e015962c3601cfa30e487380be4
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-09-01 09:59:04 +0200

    chore: update is admin

--- client/packages/portal-core/src/admin/hooks/useIsAdmin.ts
+++ client/packages/portal-core/src/admin/hooks/useIsAdmin.ts
@@ -2,34 +2,60 @@ import { useFramework } from '@equinor/fusion-framework-react';
 import { useCallback, useEffect, useState } from 'react';
 import { usePortalConfig } from '../../hooks/use-portal-config';
 import { AuthenticationResult } from '@azure/msal-browser';
-import jwt_decode from 'jwt-decode';
+import jvt_decode from 'jwt-decode';
 
-type DecodedToken = {
-	roles: string[];
+type IdTokenClaims = {
+	roles?: string[];
 };
+
+const b64DecodeUnicode = (str: string) =>
+	decodeURIComponent(
+		Array.prototype.map.call(atob(str), (c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join('')
+	);
+
+const parseJwt = (token: string) =>
+	JSON.parse(b64DecodeUnicode(token.split('.')[1].replace('-', '+').replace('_', '/')));
 
 export const useIsAdmin = () => {
 	const { modules } = useFramework();
 	const [isAdmin, setIsAdmin] = useState(false);
 	const config = usePortalConfig();
 
-	const acquireToken = useCallback(async () => {
-		const token = (await modules.auth.acquireToken({
+	const isUserAdmin = useCallback(async () => {
+		// const scope =
+		// 	config.portalClient.client.defaultScopes[0].split('/').slice(0, 3)?.toString().replaceAll(',', '/') +
+		// 	'/.default';
+		// console.log(scope);
+		const authenticationResult = (await modules.auth.acquireToken({
 			scopes: config.portalClient.client.defaultScopes,
 		})) as AuthenticationResult;
+		const peopleHttpClient = await modules.serviceDiscovery.createClient('people');
 
-		// Todo: Verify scopes
-		return token.accessToken;
+		const clientId = modules.auth.defaultAccount?.localAccountId;
+		if (clientId) {
+			const user = await peopleHttpClient.json(
+				`/persons/${clientId}?api-version=3.0&$expand=positions,contracts,roles`
+			);
+			console.log('user: ', user);
+		}
+
+		// const { roles } = authenticationResult.idTokenClaims as IdTokenClaims;
+
+		// console.log('idTokenClaims: ', authenticationResult.idTokenClaims);
+		return (
+			// roles?.includes('Fusion.ProjectPortal.Admin') ||
+			(parseJwt(authenticationResult.accessToken) as IdTokenClaims).roles?.includes(
+				'Fusion.ProjectPortal.Admin'
+			) || false
+		);
 	}, []);
 
 	useEffect(() => {
-		acquireToken().then((token) => {
-			if (token) {
-				const decoded: DecodedToken = jwt_decode(token);
-				setIsAdmin(decoded.roles.includes('Fusion.ProjectPortal.Admin'));
-			}
+		isUserAdmin().then((isAdmin) => {
+			console.log('idAdmin', isAdmin);
+			setIsAdmin(isAdmin);
 		});
-	}, [acquireToken]);
+	}, [isUserAdmin]);
 
 	return isAdmin;
 };
