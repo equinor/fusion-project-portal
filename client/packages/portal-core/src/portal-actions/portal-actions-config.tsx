18be2c93dbcdc4e6c7a4273bccaeef676ee57c3e
Christopher Berge Hove <chbh@equinor.com>
2023-10-06 10:13:15 +0200

    chore: add my account portal action option (#397)
    # Description
    
    This pull request is for demo purpose and will include an incomplete
    feature that will be removed after demo.
    
    > Please include a summary of the change and which issue is fixed.
    List any external dependencies that are required for this change.
    
    - [x] PR title and description are to the point and understandable
    - [x] I have performed a self-review of my own code'
    
    Please select version type the purposed change:
    - [ ] major
    - [ ] minor
    - [x] patch
    - [ ] none <!--- Creates an empty changeset --> 
    
    External Relations
    - [ ] database migration
    
    ## Changeset
    
    This pull request is for demo purpose and will include an incomplete
    feature that will be removed after demo.
    <!--- Write your changeset here -->
    
    ---------
    
    Co-authored-by: Noggling <Noggling@users.noreply.github.com>

--- client/packages/portal-core/src/portal-actions/portal-actions-config.tsx
+++ client/packages/portal-core/src/portal-actions/portal-actions-config.tsx
@@ -1,4 +1,4 @@
-import { Bookmarks, FullscreenIcon, Notification, Task } from '@equinor/portal-ui';
+import { Bookmarks, FullscreenIcon, MyAccount, Notification, Task } from '@equinor/portal-ui';
 
 import { ServiceMessageIcon, ServiceMessages, ServiceMessageTooltip } from '@equinor/service-message';
 import { NotificationBell } from '@equinor/notification';
@@ -14,14 +14,14 @@ export const actions: PortalAction[] = [
 		onClick: handleFullscreenClick,
 		topParOnly: true,
 	},
-	// {
-	// 	actionId: 'my-account',
-	// 	name: 'My Account',
-	// 	color: '#258800',
-	// 	icon: 'account_circle',
-	// 	component: MyAccount,
-	// 	dropDownOnly: true,
-	// },
+	{
+		actionId: 'my-account',
+		name: 'My Account',
+		color: '#258800',
+		icon: 'account_circle',
+		component: MyAccount,
+		dropDownOnly: true,
+	},
 	{
 		actionId: 'bookmarks',
 		name: 'Bookmarks',
