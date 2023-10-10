b0e4d548c47cc6215a709e1e048c75b66c47cc6b
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-10-10 12:21:14 +0200

    fix: enforce context selection to display application list in menu

--- client/packages/portal-ui/src/info-message/InfoMessage.tsx
+++ client/packages/portal-ui/src/info-message/InfoMessage.tsx
@@ -3,23 +3,29 @@ import { tokens } from '@equinor/eds-tokens';
 import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
-const StylesWrapper = styled.div`
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	padding: 20% 0;
-	justify-content: center;
-	place-self: auto center;
-`;
+const Style = {
+	Wrapper: styled.div`
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 20% 0;
+		justify-content: center;
+		place-self: auto center;
+	`,
+	Typography: styled(Typography)`
+		padding-top: 1rem;
+		text-align: center;
+	`,
+};
 
 export function InfoMessage({ children }: PropsWithChildren) {
 	return (
-		<StylesWrapper>
+		<Style.Wrapper>
 			<Icon size={48} color={tokens.colors.text.static_icons__tertiary.hex} name="info_circle" />
-			<Typography color={tokens.colors.text.static_icons__tertiary.hex} variant="h6">
+			<Style.Typography color={tokens.colors.text.static_icons__tertiary.hex} variant="h6">
 				{children}
-			</Typography>
-		</StylesWrapper>
+			</Style.Typography>
+		</Style.Wrapper>
 	);
 }
