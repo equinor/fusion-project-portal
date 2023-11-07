cece45459ad5e8bbd50ef1a828d59d8880eed931
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-11-07 11:49:20 +0100

    chore: test configuration for components project

--- client/packages/components/vite.config.ts
+++ client/packages/components/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
 	test: {
 		environment: 'jsdom',
 		globals: true,
+		setupFiles: ['.config/test-setup.ts'],
+	},
+	resolve: {
+		alias: {
+			'@portal/types': path.resolve(__dirname, '../types/src/index.ts'),
+			'@portal/ui': path.resolve(__dirname, '../ui/src/index.ts'),
+			'@portal/utils': path.resolve(__dirname, '../utils/src/index.ts'),
+		},
 	},
 });
