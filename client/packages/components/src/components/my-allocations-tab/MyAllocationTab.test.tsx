55417074afd234f5ad04a9dc3ec87e2dd349649c
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-11-07 15:11:08 +0100

    feat: added no content to allocations tab plus added test

---
+++ client/packages/components/src/components/my-allocations-tab/MyAllocationTab.test.tsx
@@ -0,0 +1,110 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { it, describe, expect, vi } from 'vitest';
+import { MyAllocationTab } from './MyAllocationTab';
+import { PersonPosition } from '@portal/types';
+import '@testing-library/jest-dom';
+import { PortalConfig } from '@equinor/portal-core';
+import { getFusionPortalURL } from '@portal/utils';
+
+const position1: PersonPosition = {
+	positionId: '1a97ae07',
+	positionExternalId: '0000',
+	id: 'e6837ff2',
+	parentPositionId: 'c5b2ab3b',
+	taskOwnerIds: [],
+	name: 'IT Software developer',
+	obs: '',
+	basePosition: {
+		id: 'bae824d2',
+		name: 'IT Software developer ',
+		type: 'TDI',
+		discipline: 'Information Technology',
+	},
+	project: {
+		id: '4cd6313d-ebeb-47e1-bdf4-51e427795e3a',
+		name: 'Fusion Test Project',
+		domainId: '0000',
+		type: 'PRD',
+	},
+	isTaskOwner: false,
+	appliesFrom: new Date('2023-08-30T00:00:00'),
+	appliesTo: new Date('2024-03-01T00:00:00'),
+	workload: 50,
+};
+
+window._config_ = { fusionLegacyEnvIdentifier: 'CI' } as PortalConfig;
+
+describe('MyAllocationTab', () => {
+	it('renders My Position tab with positions', async () => {
+		const positions = [position1];
+
+		render(
+			<MyAllocationTab
+				onClick={() => {
+					//
+				}}
+				positions={positions}
+			/>
+		);
+
+		const titleElement = screen.getByText('My Allocation');
+		expect(titleElement).toBeInTheDocument();
+
+		const roleElement = screen.getByText('IT Software developer');
+		expect(roleElement).toBeInTheDocument();
+	});
+
+	it('rendersMy Position tab with no positions', () => {
+		const positions: PersonPosition[] = [];
+
+		render(
+			<MyAllocationTab
+				onClick={() => {
+					//
+				}}
+				positions={positions}
+			/>
+		);
+
+		const titleElement = screen.getByText('My Allocation');
+		expect(titleElement).toBeInTheDocument();
+
+		const noRolesMessage = screen.getByText('You have no allocations');
+		expect(noRolesMessage).toBeInTheDocument();
+	});
+
+	it('calls the onClick function when the back button is clicked', () => {
+		const onClickMock = vi.fn();
+		render(<MyAllocationTab onClick={onClickMock} />);
+
+		const backButton = screen.getByRole('button', { name: 'Back' });
+		fireEvent.click(backButton);
+
+		expect(onClickMock).toHaveBeenCalled();
+	});
+
+	it('calls the the position and project link then links are clicked', () => {
+		const positions = [position1];
+		vi.mock('@portal/utils', (rest) => ({
+			...rest,
+			getFusionPortalURL: vi.fn(() => 'https://fusion-s-portal-ci.azurewebsites.net'),
+		}));
+
+		render(
+			<MyAllocationTab
+				onClick={() => {
+					//
+				}}
+				positions={positions}
+			/>
+		);
+
+		const projectLink = screen.getByRole('project-link', { name: position1.project.name });
+		fireEvent.click(projectLink);
+		const positionLink = screen.getByRole('position-link', { name: position1.name });
+		fireEvent.click(positionLink);
+
+		expect(getFusionPortalURL).toHaveBeenCalled();
+		expect(getFusionPortalURL).toBeCalledTimes(4);
+	});
+});
