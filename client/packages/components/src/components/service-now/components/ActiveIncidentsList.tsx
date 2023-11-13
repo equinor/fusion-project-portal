ae608a3b2443befc81110837353077d34a299f59
Christopher Berge Hove <christopher.hove@bouvet.no>
2023-11-13 11:05:35 +0100

    feat: incident item style

--- client/packages/components/src/components/service-now/components/ActiveIncidentsList.tsx
+++ client/packages/components/src/components/service-now/components/ActiveIncidentsList.tsx
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 import { PortalMessage } from '@portal/ui';
 import { useGetServiceNowIncidents } from '../hooks/use-service-now-query';
 import { ProgressLoader } from '@equinor/portal-ui';
-import { Button, Label, Typography } from '@equinor/eds-core-react';
+import { Button, Icon, Label, Typography } from '@equinor/eds-core-react';
+import { tokens } from '@equinor/eds-tokens';
+import { external_link } from '@equinor/eds-icons';
 
 export const Style = {
 	Wrapper: styled.div`
@@ -13,7 +15,47 @@ export const Style = {
 	IncidentsWrapper: styled.div`
 		padding: 1rem 0rem;
 		max-height: 65vh;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 	`,
+	IncidentItem: styled.div`
+		display: flex;
+		position: relative;
+		box-shadow: 0 2px 5px rgb(0 0 0 / 10%), 0 3px 4px rgb(0 0 0 / 10%), 0 2px 4px rgb(0 0 0 / 0%);
+		width: calc(100% - 3rem);
+		padding: 1rem;
+		align-items: center;
+		justify-content: space-between;
+	`,
+	IncidentIndicator: styled.span<{ color: string }>`
+		position: absolute;
+		display: block;
+		left: 0;
+		width: 8px;
+		height: 100%;
+		background-color: ${({ color }) => color};
+		border-top-left-radius: 4px;
+		border-bottom-left-radius: 4px;
+	`,
+	IncidentContent: styled.div`
+		padding-left: 1rem;
+	`,
+};
+
+export const getIncidentColorByState = (state: string): string => {
+	switch (state) {
+		case 'Open':
+			return `${tokens.colors.interactive.danger__resting.hex}`;
+		case 'Work in Progress':
+			return `${tokens.colors.interactive.secondary__resting.hex}`;
+		case 'Pending Vendor':
+			return `${tokens.colors.interactive.warning__resting.hex}`;
+		case 'Solution Proposed':
+			return `${tokens.colors.interactive.success__resting.hex}`;
+		default:
+			return `${tokens.colors.interactive.disabled__fill.hex}`;
+	}
 };
 
 export const ActiveIncidentsList = () => {
@@ -41,14 +83,22 @@ export const ActiveIncidentsList = () => {
 				<Style.IncidentsWrapper>
 					{data.map((item) => {
 						return (
-							<div key={item.id}>
-								<Typography variant="h6">{item.shortDescription}</Typography>
-								<Label label={item.state}></Label>
+							<Style.IncidentItem key={item.id}>
+								<Style.IncidentIndicator color={getIncidentColorByState(item.state)} />
+								<div>
+									<Button as={'a'} variant="ghost" href={item.link} target="_blank" fullWidth>
+										{item.number}
+									</Button>
 
-								<Button as={'a'} variant="ghost" href={item.link} target="_blank">
-									{item.number}
+									<Style.IncidentContent>
+										<Typography variant="h6">{item.shortDescription}</Typography>
+										<Typography variant="overline">Status: {item.state}</Typography>
+									</Style.IncidentContent>
+								</div>
+								<Button variant="ghost_icon" as={'a'} href={item.link} target="_blank">
+									<Icon data={external_link} />
 								</Button>
-							</div>
+							</Style.IncidentItem>
 						);
 					})}
 				</Style.IncidentsWrapper>
