2661ccd8b2aa668325820f2c4c0c66522e917896
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-01-15 12:11:07 +0100

    chore: count and cleanup

--- client/packages/components/src/components/task/Tasks.tsx
+++ client/packages/components/src/components/task/Tasks.tsx
@@ -2,43 +2,11 @@ import { Tabs } from '@equinor/eds-core-react';
 import { FC, useState } from 'react';
 import styled from 'styled-components';
 
-import { FusionTask } from './types/fusion-task';
 import { useAssignment } from './hooks/use-assignment';
-import { Task } from './types/task';
+
 import { TabNav } from './components/TaskNav';
 import { TaskList } from './components/TaskList';
-
-// const StyledAccordianItem = styled(Accordion.Item)`
-// 	border: none;
-// `;
-
-// const StyledAccordianHeader = styled(Accordion.Header)`
-// 	border: none;
-// `;
-
-// const StyledAccordianPanel = styled(Accordion.Panel)`
-// 	border: none;
-// `;
-// const StyledAccordian = styled(Accordion)`
-// 	height: 100%;
-// `;
-
-const groupOption = {
-	Category: (task: FusionTask) => task.category,
-	'Owner Application': (task: FusionTask) => task.ownerApplication?.title || 'Unknown',
-	Priority: (task: FusionTask) => task.priority,
-	'Source System': (task: FusionTask) => task.sourceSystem?.subSystem || 'Unknown',
-	State: (task: FusionTask) => task.state,
-	TaskMode: (task: FusionTask) => task.taskMode,
-	Type: (task: FusionTask) => task.type || 'Unknown',
-};
-
-// const groupBy = (arr: Task[], getKey: (task: Task) => string) =>
-// 	arr.reduce((groups, item) => {
-// 		const key = getKey(item);
-// 		(groups[key] ||= []).push(item);
-// 		return groups;
-// 	}, {} as Record<PropertyKey, Task[]>);
+import { useTaskCount } from './hooks/use-task-count';
 
 interface TasksProps {
 	maxDisplay?: number;
@@ -65,7 +33,6 @@ const Style = {
 };
 
 export const Tasks: FC<TasksProps> = ({ maxDisplay }) => {
-	const [groupedBy, setGroupedBy] = useState<keyof typeof groupOption>('Category');
 	const assignments = useAssignment().slice(0, maxDisplay ? maxDisplay : -1);
 
 	const [activeTab, setActiveTab] = useState(0);
@@ -73,28 +40,20 @@ export const Tasks: FC<TasksProps> = ({ maxDisplay }) => {
 	const handleChange = (index: number) => {
 		setActiveTab(index);
 	};
+	const count = useTaskCount(assignments);
 
 	return (
 		<Style.Wrapper>
-			<Style.Header>
-				{/* <Autocomplete
-					options={Object.keys(groupOption)}
-					autoWidth={true}
-					hideClearButton={true}
-					label={'Group by'}
-					selectedOptions={[groupedBy]}
-					onOptionsChange={(changes) => setGroupedBy(changes.selectedItems[0] as keyof typeof groupOption)}
-				/> */}
-			</Style.Header>
+			<Style.Header></Style.Header>
 			<Style.TaskListWrapper>
 				<Tabs activeTab={activeTab} onChange={handleChange}>
 					<TabNav>
-						<Tabs.Tab>All</Tabs.Tab>
-						<Tabs.Tab>Meetings </Tabs.Tab>
-						<Tabs.Tab>Review </Tabs.Tab>
-						<Tabs.Tab>PIMS </Tabs.Tab>
-						<Tabs.Tab>ProCoSys </Tabs.Tab>
-						<Tabs.Tab>Query & NC Request </Tabs.Tab>
+						<Tabs.Tab>All ({assignments.length})</Tabs.Tab>
+						<Tabs.Tab>Meetings ({count.Meetings}) </Tabs.Tab>
+						<Tabs.Tab>Review ({count.Review})</Tabs.Tab>
+						<Tabs.Tab>PIMS ({count.PIMS})</Tabs.Tab>
+						<Tabs.Tab>ProCoSys ({count.ProCoSys})</Tabs.Tab>
+						<Tabs.Tab>Query & NC Request ({count['Query & NC Request']})</Tabs.Tab>
 					</TabNav>
 					<Tabs.Panels>
 						<Tabs.Panel>
