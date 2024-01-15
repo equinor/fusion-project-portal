4c03f95f2ed80e857165d9b2291499e1486233c6
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-01-15 14:04:07 +0100

    fix: bookmark app and loading

--- client/packages/portal-ui/src/bookmarks/Bookmarks.tsx
+++ client/packages/portal-ui/src/bookmarks/Bookmarks.tsx
@@ -1,15 +1,32 @@
+import { BookmarkModule } from '@equinor/fusion-framework-module-bookmark';
 import { Bookmark } from '@equinor/fusion-framework-react-components-bookmark';
-import { useHasBookmark } from '@equinor/fusion-framework-react-module-bookmark/portal';
+import {} from '@equinor/fusion-framework-react-module-bookmark/portal';
 import { PortalActionProps } from '@equinor/portal-core';
 
 import { SideSheet } from '@equinor/fusion-react-side-sheet';
 import { useFramework } from '@equinor/fusion-framework-react';
 import { Button, Icon } from '@equinor/eds-core-react';
+import { useEffect, useState } from 'react';
 
 export function Bookmarks({ action, onClose, open }: PortalActionProps) {
-	const hasBookmark = useHasBookmark();
+	const { event, bookmark } = useFramework<[BookmarkModule]>().modules;
 
-	const { event } = useFramework().modules;
+	const [hasBookmark, setHasBookmark] = useState(false);
+
+	useEffect(() => {
+		const subOnBookmarkChanged = event.addEventListener('onBookmarkChanged', () => {
+			setHasBookmark(false);
+		});
+		const subOnAddCreator = event.addEventListener('onAddCreator', () => {
+			setHasBookmark(true);
+		});
+		setHasBookmark(Object.keys(bookmark.bookmarkCreators).length > 0);
+		return () => {
+			subOnBookmarkChanged();
+			subOnAddCreator();
+			setHasBookmark(false);
+		};
+	}, [event]);
 
 	return (
 		<SideSheet isOpen={open} onClose={onClose} isDismissable={true} minWidth={action.minWidth}>
