80ce0b0c859b53956ae4a6e17f43a9ee83f7d6b5
Christopher Berge Hove <chbh@equinor.com>
2024-01-18 15:05:48 +0100

    fix: fix for review and url handling on legacy apps (#522)
    # Description
     fixing the landing of review and url handling for legacy applications
    
    
    > Please include a summary of the change and which issue is fixed.
    List any external dependencies that are required for this change.
    
    - [x] PR title and description are to the point and understandable
    - [x] I have performed a self-review of my own code'
    
    Please select version type the purposed change:
    - [ ] major
    - [ ] minor
    - [x] patch
    - [ ] none <!--- Creates an empty changeset --> 
    
    External Relations
    - [ ] database migration
    
    
    ## Changeset
     fixing the landing of review and url handling for legacy applications
    <!--- Write your changeset here -->
    
    ---------
    
    Co-authored-by: Noggling <Noggling@users.noreply.github.com>

--- client/packages/portal-client/src/pages/AppPage/AppPage.tsx
+++ client/packages/portal-client/src/pages/AppPage/AppPage.tsx
@@ -1,30 +1,45 @@
-import { useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-
-import { useAppModule, isLegacyFusionApplication } from '@portal/core';
-
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { verifyContextInURL } from '../../utils/context-utils';
 import { AppProvider } from '../../components/app-provider/AppProvider';
 import { useFrameworkCurrentContext } from '@equinor/portal-core';
+import { useFramework } from '@equinor/fusion-framework-react';
+import { AppModule, AppModulesInstance } from '@equinor/fusion-framework-module-app';
+import { ContextModule } from '@equinor/fusion-framework-module-context';
+import { NavigationModule } from '@equinor/fusion-framework-module-navigation';
 
-export const AppPage = () => {
-	const { appKey, contextId } = useParams();
+const useAppContextHandler = () => {
+	const { contextId } = useParams();
+	const framework = useFramework<[AppModule, NavigationModule]>();
 
 	const context = useFrameworkCurrentContext();
 
-	const navigate = useNavigate();
-
-	const { appModule } = useAppModule();
+	useEffect(() => {
+		return framework.modules.event.addEventListener('onReactAppLoaded', (e) => {
+			const contextProvider = (e.detail.modules as AppModulesInstance<[ContextModule]>).context;
+			if (contextProvider && context && !contextId && !verifyContextInURL(window.location.pathname)) {
+				framework.modules.navigation.replace(`${window.location.pathname}${context.id}`);
+			}
+		});
+	}, [framework, contextId, context]);
 
 	useEffect(() => {
-		// Todo use context util
-		if (context && !contextId && !verifyContextInURL(window.location.pathname)) {
-			isLegacyFusionApplication({ appKey, appProvider: appModule }).then(
-				// navigation should only be performed on new applications and not legacy applications
-				(isLegacy) => !isLegacy && navigate(`${window.location.pathname}${context.id}`)
-			);
-		}
-	}, [appKey, context, contextId]);
+		return framework.modules.event.addEventListener('onReactAppLoaded', (e) => {
+			console.debug('onReactAppLoaded', 'using legacy context hack method');
+			const contextProvider = (e.detail.modules as AppModulesInstance<[ContextModule]>).context;
+			if (contextProvider && !contextProvider.currentContext && context) {
+				contextProvider.setCurrentContextAsync(context);
+			}
+		});
+	}, [framework, context]);
+};
+
+export const AppPage = () => {
+	const { appKey } = useParams();
+
+	useAppContextHandler();
+
+	const context = useFrameworkCurrentContext();
 
 	return <AppProvider hasContext={Boolean(context)} appKey={appKey} />;
 };
