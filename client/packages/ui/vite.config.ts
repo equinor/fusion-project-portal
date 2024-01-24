eafb2989b9374fb6a164904b51b4fdbaf4c20c3c
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-01-24 18:31:58 +0100

    fix: vite config fails

--- client/packages/ui/vite.config.ts
+++ client/packages/ui/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
 import path from 'path';
-import tsconfig from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfig()],
 	test: {
 		environment: 'jsdom',
 		globals: true,
