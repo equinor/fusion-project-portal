e6be5e94fb4963f78c1046d881a3b51a1615e5ab
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-02-12 10:55:59 +0100

    chore: add back milestones when cc tab feature is disabled

--- client/packages/portal-pages/src/pages/project-page/Overvirew.tsx
+++ client/packages/portal-pages/src/pages/project-page/Overvirew.tsx
@@ -6,9 +6,12 @@ import { Styles } from './ProjectPage';
 import AppSearch from './components/app-search';
 import { useFrameworkFeature } from '@equinor/fusion-framework-react/feature-flag';
 import { Phases } from './components/phases/Phases';
+import { Milestones } from './components/milestones/Milestones';
 
 export const Overview = () => {
 	const { feature } = useFrameworkFeature('app-search');
+
+	const { feature: ccTabFeature } = useFrameworkFeature('cc-tab');
 	return (
 		<Styles.Row>
 			<Styles.Col>
@@ -17,6 +20,8 @@ export const Overview = () => {
 			<Styles.Col>
 				<Phases />
 				<Favorites />
+				{/* Todo remove when cc tab is not in feature flag mode */}
+				{ccTabFeature?.enabled === false && <Milestones />}
 				{feature?.enabled && <AppSearch />}
 				<Contracts />
 			</Styles.Col>
