700c1607c7557d6fe834a1e74d8f24fae228f586
Jostein <jtakl@equinor.com>
2024-07-04 11:03:15 +0200

    add get post and delete to context-type

---
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/ViewModels/ContextType/ApiAddContextTypeRequest.cs
@@ -0,0 +1,26 @@
+﻿using Equinor.ProjectExecutionPortal.Application.Commands.ContextTypes.AddContextType;
+using FluentValidation;
+
+namespace Equinor.ProjectExecutionPortal.WebApi.ViewModels.ContextType
+{
+    public class ApiAddContextTypeRequest
+    {
+        public string Type { get; set; } = null!;
+
+        public AddContextTypeCommand ToCommand()
+        {
+            return new AddContextTypeCommand(Type);
+        }
+
+        public class ApiAddContextTypeRequestValidator : AbstractValidator<ApiAddContextTypeRequest>
+        {
+            public ApiAddContextTypeRequestValidator()
+            {
+                RuleFor(x => x.Type)
+                    .NotEmpty()
+                    .NotContainScriptTag()
+                    .WithMessage("Type required");
+            }
+        }
+    }
+}
