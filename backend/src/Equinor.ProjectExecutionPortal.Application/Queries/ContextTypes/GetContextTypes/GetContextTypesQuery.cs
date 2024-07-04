f512441ff6fcd523a9114ded0a17718c9b6bf2e3
Jostein Taklo <jostein.taklo@gmail.com>
2024-07-04 15:16:56 +0200

    feat: get post and delete to context-type (#668)
    Co-authored-by: Jossilainen <Jossilainen@users.noreply.github.com>

---
+++ backend/src/Equinor.ProjectExecutionPortal.Application/Queries/ContextTypes/GetContextTypes/GetContextTypesQuery.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Equinor.ProjectExecutionPortal.Domain.Infrastructure;
+using Equinor.ProjectExecutionPortal.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Equinor.ProjectExecutionPortal.Application.Queries.ContextTypes.GetContextTypes;
+
+    public class GetContextTypesQuery : QueryBase<IList<ContextTypeDto>>
+    {
+        public GetContextTypesQuery()
+        {
+        }
+
+        public class Handler : IRequestHandler<GetContextTypesQuery, IList<ContextTypeDto>>
+        {
+            private readonly IReadWriteContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(IReadWriteContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<IList<ContextTypeDto>> Handle(GetContextTypesQuery request,
+                CancellationToken cancellationToken)
+            {
+                var entity = await _context.Set<Domain.Entities.ContextType>()
+                    .AsNoTracking()
+                    .ToListAsync(cancellationToken);
+
+                var contextTypes = _mapper.Map<List<Domain.Entities.ContextType>, List<ContextTypeDto>>(entity);
+
+                return contextTypes;
+            }
+        }
+
+    }
+
+
