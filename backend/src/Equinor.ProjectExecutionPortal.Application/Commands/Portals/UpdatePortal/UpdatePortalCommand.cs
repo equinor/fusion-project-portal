e4c43940a5abe73632544eef9b3f90925f5da637
Jostein Taklo <jostein.taklo@gmail.com>
2024-08-14 15:01:01 +0200

    feat: add context-types to put-endpoint (#700)
    Co-authored-by: Jossilainen <Jossilainen@users.noreply.github.com>

--- backend/src/Equinor.ProjectExecutionPortal.Application/Commands/Portals/UpdatePortal/UpdatePortalCommand.cs
+++ backend/src/Equinor.ProjectExecutionPortal.Application/Commands/Portals/UpdatePortal/UpdatePortalCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Equinor.ProjectExecutionPortal.Application.Helpers;
+using Equinor.ProjectExecutionPortal.Application.Services.ContextTypeService;
 using Equinor.ProjectExecutionPortal.Domain.Common.Exceptions;
 using Equinor.ProjectExecutionPortal.Domain.Entities;
 using Equinor.ProjectExecutionPortal.Infrastructure;
@@ -9,7 +10,7 @@ namespace Equinor.ProjectExecutionPortal.Application.Commands.Portals.UpdatePort
 
 public class UpdatePortalCommand : IRequest<Guid>
 {
-    public UpdatePortalCommand(Guid id, string name, string shortName, string subText, string? description, int order, string icon)
+    public UpdatePortalCommand(Guid id, string name, string shortName, string subText, string? description, int order, string icon, IList<string>? contextTypes)
     {
         Id = id;
         Name = name;
@@ -18,6 +19,7 @@ public class UpdatePortalCommand : IRequest<Guid>
         Description = description;
         Order = order;
         Icon = icon;
+        ContextTypes = contextTypes;
     }
 
     public Guid Id { get; }
@@ -27,19 +29,23 @@ public class UpdatePortalCommand : IRequest<Guid>
     public string? Description { get; set; }
     public int Order { get; }
     public string Icon { get; }
+    public IList<string>? ContextTypes { get; set; }
 
     public class Handler : IRequestHandler<UpdatePortalCommand, Guid>
     {
         private readonly IReadWriteContext _readWriteContext;
+        private readonly IContextTypeService _contextTypeService;
 
-        public Handler(IReadWriteContext readWriteContext)
+        public Handler(IReadWriteContext readWriteContext, IContextTypeService contextTypeService)
         {
             _readWriteContext = readWriteContext;
+            _contextTypeService = contextTypeService;
         }
 
         public async Task<Guid> Handle(UpdatePortalCommand command, CancellationToken cancellationToken)
         {
             var entity = await _readWriteContext.Set<Portal>()
+                .Include(x => x.ContextTypes)
                 .FirstOrDefaultAsync(x => x.Id == command.Id, cancellationToken);
 
             if (entity == null)
@@ -50,6 +56,8 @@ public class UpdatePortalCommand : IRequest<Guid>
             var slug = SlugHelper.Sluggify(command.Name);
 
             entity.Update(slug, command.Name, command.ShortName, command.SubText, command.Description, command.Order, command.Icon);
+
+            entity.AddContextTypes(await _contextTypeService.GetContextTypesByContextTypeKey(command.ContextTypes, cancellationToken));
 
             await _readWriteContext.SaveChangesAsync(cancellationToken);
 
