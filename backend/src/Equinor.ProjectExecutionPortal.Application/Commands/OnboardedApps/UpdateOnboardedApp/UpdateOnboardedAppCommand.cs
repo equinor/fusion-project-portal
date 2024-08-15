543aa1e9ffef218643083b598bd463164852d4a1
Jostein <jtakl@equinor.com>
2024-08-15 12:35:47 +0200

    add context-types to put endpoint

--- backend/src/Equinor.ProjectExecutionPortal.Application/Commands/OnboardedApps/UpdateOnboardedApp/UpdateOnboardedAppCommand.cs
+++ backend/src/Equinor.ProjectExecutionPortal.Application/Commands/OnboardedApps/UpdateOnboardedApp/UpdateOnboardedAppCommand.cs
@@ -1,4 +1,5 @@
-﻿using Equinor.ProjectExecutionPortal.Domain.Common.Exceptions;
+﻿using Equinor.ProjectExecutionPortal.Application.Services.ContextTypeService;
+using Equinor.ProjectExecutionPortal.Domain.Common.Exceptions;
 using Equinor.ProjectExecutionPortal.Domain.Entities;
 using Equinor.ProjectExecutionPortal.Infrastructure;
 using MediatR;
@@ -8,27 +9,32 @@ namespace Equinor.ProjectExecutionPortal.Application.Commands.OnboardedApps.Upda
 
 public class UpdateOnboardedAppCommand : IRequest<Guid>
 {
-    public UpdateOnboardedAppCommand(string appKey, bool isLegacy)
+    public UpdateOnboardedAppCommand(string appKey, bool isLegacy, IList<string>? contextTypes)
     {
         AppKey = appKey;
         IsLegacy = isLegacy;
+        ContextTypes = contextTypes;
     }
 
     public string AppKey { get; }
     public bool IsLegacy { get; }
+    public IList<string>? ContextTypes { get; set; }
 
     public class Handler : IRequestHandler<UpdateOnboardedAppCommand, Guid>
     {
         private readonly IReadWriteContext _readWriteContext;
+        private readonly IContextTypeService _contextTypeService;
 
-        public Handler(IReadWriteContext readWriteContext)
+        public Handler(IReadWriteContext readWriteContext, IContextTypeService contextTypeService)
         {
             _readWriteContext = readWriteContext;
+            _contextTypeService = contextTypeService;
         }
 
         public async Task<Guid> Handle(UpdateOnboardedAppCommand command, CancellationToken cancellationToken)
         {
             var entity = await _readWriteContext.Set<OnboardedApp>()
+                .Include(x => x.ContextTypes)
                 .FirstOrDefaultAsync(x => x.AppKey == command.AppKey, cancellationToken);
 
             if (entity == null)
@@ -37,6 +43,15 @@ public class UpdateOnboardedAppCommand : IRequest<Guid>
             }
 
             entity.Update(command.IsLegacy);
+
+            try
+            {
+                entity.AddContextTypes(await _contextTypeService.GetContextTypesByContextTypeKey(command.ContextTypes, cancellationToken));
+            }
+            catch (InvalidActionException ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
 
             await _readWriteContext.SaveChangesAsync(cancellationToken);
 
--- backend/src/Equinor.ProjectExecutionPortal.WebApi/ViewModels/OnboardedApp/ApiUpdateOnboardedAppRequest.cs
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/ViewModels/OnboardedApp/ApiUpdateOnboardedAppRequest.cs
@@ -1,4 +1,5 @@
 ﻿using Equinor.ProjectExecutionPortal.Application.Commands.OnboardedApps.UpdateOnboardedApp;
+using Equinor.ProjectExecutionPortal.Application.Queries.OnboardedContexts.GetOnboardedContext;
 using FluentValidation;
 
 namespace Equinor.ProjectExecutionPortal.WebApi.ViewModels.OnboardedApp
@@ -6,10 +7,11 @@ namespace Equinor.ProjectExecutionPortal.WebApi.ViewModels.OnboardedApp
     public class ApiUpdateOnboardedAppRequest
     {
         public bool IsLegacy { get; set; }
+        public IList<string>? ContextTypes { get; set; }
 
         public UpdateOnboardedAppCommand ToCommand(string appKey)
         {
-            return new UpdateOnboardedAppCommand(appKey, IsLegacy);
+            return new UpdateOnboardedAppCommand(appKey, IsLegacy, ContextTypes);
         }
 
         public class ApiUpdateOnboardedAppRequestValidator : AbstractValidator<ApiUpdateOnboardedAppRequest>
