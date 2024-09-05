5dd85934a810a0f15113044ddd99bddac0ec39b4
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-09-05 14:29:23 +0200

    chore: Update PortalTable component to use styled-components for consistent styling

--- client/apps/portal-administration/src/components/Portals/PortalTable.tsx
+++ client/apps/portal-administration/src/components/Portals/PortalTable.tsx
@@ -1,127 +1,160 @@
-import styled from "styled-components";
-import { ClientGrid } from "@equinor/workspace-ag-grid";
-import { Portal } from "../../types";
-import { useNavigate } from "react-router-dom";
-import { CustomCellRendererProps } from "@ag-grid-community/react";
-import { Button, Chip, Icon } from "@equinor/eds-core-react";
-import { edit, delete_to_trash } from "@equinor/eds-icons";
-import { useRef } from "react";
-import { useResizeObserver } from "../../hooks/use-resise-observer";
-
+import styled from 'styled-components';
+import { ClientGrid } from '@equinor/workspace-ag-grid';
+import { Portal } from '../../types';
+import { useNavigate } from 'react-router-dom';
+import { CustomCellRendererProps } from '@ag-grid-community/react';
+import { Button, Chip, Icon } from '@equinor/eds-core-react';
+import { edit, delete_to_trash } from '@equinor/eds-icons';
+import { useRef } from 'react';
+import { useResizeObserver } from '../../hooks/use-resise-observer';
+import * as AllIcons from '@equinor/eds-icons';
 const Styles = {
-  CellWrapper: styled.div`
-    display: flex;
-    justify-content: flex-end;
-  `,
-  Chip: styled(Chip)`
-    margin-top: 0.5rem;
-  `,
-  TableContent: styled.div`
-    position: relative;
-    width: calc(100% - 2rem);
-    height: 100%;
-  `,
+	CellWrapper: styled.div`
+		display: flex;
+		justify-content: flex-end;
+	`,
+	Chip: styled(Chip)`
+		margin-top: 0.5rem;
+	`,
+	TableContent: styled.div`
+		position: relative;
+		width: calc(100% - 2rem);
+		height: 100%;
+	`,
+	Icon: styled.span`
+		> svg {
+			width: 25px;
+		}
+	`,
 };
 
 export function PortalTable({ portalsData }: { portalsData?: Portal[] }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const ref = useRef(null);
-  const [_, height] = useResizeObserver(ref);
+	const ref = useRef(null);
+	const [_, height] = useResizeObserver(ref);
 
-  return (
-    <Styles.TableContent ref={ref}>
-      <ClientGrid<Portal>
-        height={height - 208}
-        rowData={portalsData || []}
-        enableCellTextSelection
-        ensureDomOrder
-        rowHeight={36}
-        autoSizeStrategy={{
-          type: "fitGridWidth",
-          defaultMinWidth: 80,
-          defaultMaxWidth: 300,
-        }}
-        colDefs={[
-          {
-            field: "id",
-            headerName: "Id",
-            hide: true,
-          },
-          {
-            field: "name",
-            headerName: "Name",
-            onCellClicked: (event) => {
-              navigate(`/portals/${event.data?.id}/overview`);
-            },
-          },
-          {
-            field: "subtext",
-            headerName: "Sub Text",
-          },
-          {
-            field: "shortName",
-            headerName: "Short Name",
-          },
-          {
-            field: "description",
-            headerName: "Description",
-          },
-          {
-            field: "contexts",
-            headerName: "Contexts Types",
+	return (
+		<Styles.TableContent ref={ref}>
+			<ClientGrid<Portal>
+				height={height - 208}
+				rowData={portalsData || []}
+				enableCellTextSelection
+				ensureDomOrder
+				rowHeight={36}
+				autoSizeStrategy={{
+					type: 'fitGridWidth',
+					defaultMinWidth: 80,
+					defaultMaxWidth: 300,
+				}}
+				colDefs={[
+					{
+						field: 'id',
+						headerName: 'Id',
+						hide: true,
+					},
+					{
+						field: 'icon',
+						headerName: 'Icon',
+						maxWidth: 70,
+						cellRenderer: (
+							params: CustomCellRendererProps<{
+								icon: string;
+								id: string;
+							}>
+						) => {
+							if (!params.data?.icon) {
+								return null;
+							}
+							return (
+								<div key={params.data?.id}>
+									{params.data?.icon && Object.keys(AllIcons).includes(params.data?.icon) ? (
+										<Icon name={params.data?.icon} size={48} />
+									) : (
+										<Styles.Icon
+											dangerouslySetInnerHTML={{
+												__html: params.data?.icon.replace(/\s+/g, ' ').trim(),
+											}}
+										/>
+									)}
+								</div>
+							);
+						},
+					},
+					{
+						field: 'name',
+						headerName: 'Name',
+						onCellClicked: (event) => {
+							navigate(`/portals/${event.data?.id}/overview`);
+						},
+					},
+					{
+						field: 'subtext',
+						headerName: 'Sub Text',
+					},
+					{
+						field: 'shortName',
+						headerName: 'Short Name',
+					},
+					{
+						field: 'description',
+						headerName: 'Description',
+					},
+					{
+						field: 'contexts',
+						headerName: 'Contexts Types',
 
-            cellRenderer: (
-              params: CustomCellRendererProps<{
-                contextTypes: string[];
-                id: string;
-              }>
-            ) => {
-              return (
-                <Styles.CellWrapper key={params.context?.id}>
-                  {params?.data?.contextTypes?.map((type) => {
-                    return (
-                      <Styles.Chip variant="default" key={type}>
-                        {type}
-                      </Styles.Chip>
-                    );
-                  })}
-                </Styles.CellWrapper>
-              );
-            },
-          },
-          {
-            field: "id",
-            headerName: "Actions",
-            cellRenderer: (params: CustomCellRendererProps<Portal>) => {
-              return (
-                <Styles.CellWrapper>
-                  <Button
-                    variant="ghost"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
+						cellRenderer: (
+							params: CustomCellRendererProps<{
+								contextTypes: string[];
+								id: string;
+							}>
+						) => {
+							return (
+								<Styles.CellWrapper key={params.context?.id}>
+									{params?.data?.contextTypes?.map((type) => {
+										return (
+											<Styles.Chip variant="default" key={type}>
+												{type}
+											</Styles.Chip>
+										);
+									})}
+								</Styles.CellWrapper>
+							);
+						},
+					},
+					{
+						field: 'id',
+						headerName: 'Actions',
+						cellRenderer: (params: CustomCellRendererProps<Portal>) => {
+							return (
+								<Styles.CellWrapper>
+									<Button
+										variant="ghost"
+										onClick={(e) => {
+											e.preventDefault();
+											e.stopPropagation();
 
-                      navigate(`/portals/${params.data?.id}/overview`);
-                    }}
-                  >
-                    <Icon data={edit} size={16} />
-                  </Button>
-                  <Button
-                    variant="ghost"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                    }}
-                  >
-                    <Icon data={delete_to_trash} size={16} />
-                  </Button>
-                </Styles.CellWrapper>
-              );
-            },
-          },
-        ]}
-      />
-    </Styles.TableContent>
-  );
+											navigate(`/portals/${params.data?.id}/overview`);
+										}}
+									>
+										<Icon data={edit} size={16} />
+									</Button>
+									<Button
+										variant="ghost"
+										onClick={(e) => {
+											e.preventDefault();
+											e.stopPropagation();
+										}}
+									>
+										<Icon data={delete_to_trash} size={16} />
+									</Button>
+								</Styles.CellWrapper>
+							);
+						},
+					},
+				]}
+			/>
+		</Styles.TableContent>
+	);
 }
--- client/apps/portal-administration/src/types/index.ts
+++ client/apps/portal-administration/src/types/index.ts
@@ -1,73 +1,73 @@
-import { AppManifest } from "@equinor/fusion-framework-react-app";
-import { Router } from "./router-config";
+import { AppManifest } from '@equinor/fusion-framework-react-app';
+import { Router } from './router-config';
 
 export type Portal = {
-  name: string;
-  shortName: string;
-  subtext: string;
-  id: string;
-  key: string;
-  type: string;
-  icon: string;
-  description: string;
-  contexts: ContextType[];
+	name: string;
+	shortName: string;
+	subtext: string;
+	id: string;
+	key: string;
+	type: string;
+	icon?: string;
+	description: string;
+	contexts: ContextType[];
 };
 
-export type CreatePortal = Omit<Portal, "id">;
+export type CreatePortal = Omit<Portal, 'id'>;
 
 export type PortalAppCreate = {
-  appKey: string;
-  removeAppFromContexts: boolean;
+	appKey: string;
+	removeAppFromContexts: boolean;
 };
 
 export type ContextType = {
-  type: string;
+	type: string;
 };
 export type OnboardedContext = {
-  title: string;
-  id: string;
-  contextId: string;
-  externalId: string;
-  type: string;
-  description: string;
+	title: string;
+	id: string;
+	contextId: string;
+	externalId: string;
+	type: string;
+	description: string;
 };
 
-export type Variant = "Warning" | "Error" | "Info" | "NoContent";
+export type Variant = 'Warning' | 'Error' | 'Info' | 'NoContent';
 
 export type Message = {
-  type?: Variant;
-  title: string;
-  messages?: string[];
+	type?: Variant;
+	title: string;
+	messages?: string[];
 };
 
 export type FormattedError = {
-  status: number;
+	status: number;
 } & Message;
 
 export type PortalApp = {
-  name: string;
-  id: string;
-  appKey: string;
-  isLegacy: boolean;
-  description: string;
-  contexts: ContextType[];
-  appInformation: { icon: string };
-  isActive?: boolean;
+	name: string;
+	id: string;
+	appKey: string;
+	isLegacy: boolean;
+	description: string;
+	contexts: ContextType[];
+	appInformation: { icon: string };
+	isActive?: boolean;
 };
 export type PortalAppMutation = {
-  contextTypes: string[];
+	contextTypes: string[];
 };
 
 export type AppManifestResponse = {
-  key: string;
-  contextTypes: ContextType[];
-  appManifest: AppManifest;
+	key: string;
+	contextTypes: ContextType[];
+	appManifest: AppManifest;
 };
 
 export type PortalConfiguration = {
-  router: Router;
+	router: Router;
 };
 
 export type PortalConfigurationEditInput = {
-  id: string;
+	id: string;
 } & PortalConfiguration;
