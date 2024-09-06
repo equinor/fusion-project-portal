61bbb3fefd7d91c4bb21914f725d08e580279aa1
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-09-06 12:49:24 +0200

    chore: Refactor OnboardApp component to use styled-components for consistent styling

--- client/apps/portal-administration/src/components/OnboardedApps/AppsTable.tsx
+++ client/apps/portal-administration/src/components/OnboardedApps/AppsTable.tsx
@@ -1,135 +1,137 @@
-import { CustomCellRendererProps } from "@ag-grid-community/react";
-import { Button, Chip, Icon } from "@equinor/eds-core-react";
-import { ClientGrid } from "@equinor/workspace-ag-grid";
-import { useState } from "react";
-import styled from "styled-components";
-import {
-  useDeleteOnboardedApp,
-  useOnboardedApps,
-} from "../../hooks/use-onboarded-apps";
-import { PortalApp, ContextType } from "../../types";
-import { AppSideSheet } from "./AppSideSheet";
-import { Loading } from "../Loading";
-import { delete_forever, delete_to_trash, edit } from "@equinor/eds-icons";
+import { CustomCellRendererProps } from '@ag-grid-community/react';
+import { Button, Chip, Icon } from '@equinor/eds-core-react';
+import { ClientGrid } from '@equinor/workspace-ag-grid';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useDeleteOnboardedApp, useOnboardedApps } from '../../hooks/use-onboarded-apps';
+import { PortalApp } from '../../types';
+import { AppSideSheet } from './AppSideSheet';
+import { Loading } from '../Loading';
+import { delete_to_trash, edit } from '@equinor/eds-icons';
+import { AgStyle } from '../AgStyle';
 
 const Styles = {
-  Wrapper: styled.div`
-    padding: 0 1rem;
-  `,
-  CellWrapper: styled.div`
-    display: flex;
-    justify-content: flex-end;
-  `,
-  Chip: styled(Chip)`
-    margin-top: 0.25rem;
-    margin-right: 0.5rem;
-  `,
+	TableContent: styled.div`
+		position: relative;
+		width: calc(100% - 2rem);
+		height: 100%;
+	`,
+	CellWrapper: styled.div`
+		display: flex;
+		justify-content: flex-end;
+	`,
+	Chip: styled(Chip)`
+		margin-top: 0.25rem;
+		margin-right: 0.5rem;
+	`,
 };
 export const AppsTable = () => {
-  const { data, isLoading } = useOnboardedApps();
-  const [selectedApp, setSelectedApp] = useState<PortalApp | undefined>();
+	const { data, isLoading } = useOnboardedApps();
+	const [selectedApp, setSelectedApp] = useState<PortalApp | undefined>();
 
-  const { mutateAsync: deleteAppByAppKey } = useDeleteOnboardedApp();
-  if (isLoading) return <Loading detail="Loading onboarded apps" />;
+	const { mutateAsync: deleteAppByAppKey } = useDeleteOnboardedApp();
+	if (isLoading) return <Loading detail="Loading onboarded apps" />;
 
-  if (!data) return <>No data provided</>;
+	if (!data) return <>No data provided</>;
 
-  return (
-    <Styles.Wrapper>
-      <AppSideSheet
-        app={selectedApp}
-        onClose={() => {
-          setSelectedApp(undefined);
-        }}
-      />
+	return (
+		<AgStyle>
+			<Styles.TableContent>
+				<AppSideSheet
+					app={selectedApp}
+					onClose={() => {
+						setSelectedApp(undefined);
+					}}
+				/>
 
-      <ClientGrid<PortalApp>
-        height={window.innerHeight - 250}
-        rowData={data}
-        enableCellTextSelection
-        ensureDomOrder
-        rowHeight={36}
-        autoSizeStrategy={{
-          type: "fitGridWidth",
-          defaultMinWidth: 80,
-          defaultMaxWidth: 300,
-        }}
-        colDefs={[
-          {
-            field: "id",
-            headerName: "Id",
-            hide: true,
-          },
-          {
-            field: "name",
-            headerName: "Name",
-          },
-          {
-            field: "appKey",
-            headerName: "App Key",
-          },
-          {
-            field: "description",
-            headerName: "Description",
-          },
+				<ClientGrid<PortalApp>
+					height={window.innerHeight - 250}
+					rowData={data}
+					enableCellTextSelection
+					ensureDomOrder
+					rowHeight={36}
+					autoSizeStrategy={{
+						type: 'fitGridWidth',
+						defaultMinWidth: 80,
+						defaultMaxWidth: 300,
+					}}
+					colDefs={[
+						{
+							field: 'id',
+							headerName: 'Id',
+							hide: true,
+						},
+						{
+							field: 'name',
+							headerName: 'Name',
+						},
+						{
+							field: 'appKey',
+							headerName: 'App Key',
+						},
+						{
+							field: 'description',
+							headerName: 'Description',
+						},
 
-          {
-            field: "contexts",
-            headerName: "Contexts Types",
+						{
+							field: 'contexts',
+							headerName: 'Contexts Types',
 
-            cellRenderer: (
-              params: CustomCellRendererProps<{
-                contextTypes: string[];
-                appKey: string;
-              }>
-            ) => {
-              return (
-                <Styles.CellWrapper key={params.context?.appKey}>
-                  {params?.data?.contextTypes?.map((type) => {
-                    return (
-                      <Styles.Chip variant="default" key={type}>
-                        {type}
-                      </Styles.Chip>
-                    );
-                  })}
-                </Styles.CellWrapper>
-              );
-            },
-          },
-          {
-            field: "appKey",
-            headerName: "Actions",
-            cellRenderer: (params: CustomCellRendererProps<PortalApp>) => {
-              return (
-                <Styles.CellWrapper key={params.context?.appKey}>
-                  <Button
-                    variant="ghost"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
+							cellRenderer: (
+								params: CustomCellRendererProps<{
+									contextTypes: string[];
+									appKey: string;
+								}>
+							) => {
+								return (
+									<Styles.CellWrapper key={params.context?.appKey}>
+										{params?.data?.contextTypes?.map((type) => {
+											return (
+												<Styles.Chip variant="default" key={type}>
+													{type}
+												</Styles.Chip>
+											);
+										})}
+									</Styles.CellWrapper>
+								);
+							},
+						},
+						{
+							field: 'appKey',
+							headerName: 'Actions',
+							cellRenderer: (params: CustomCellRendererProps<PortalApp>) => {
+								return (
+									<Styles.CellWrapper key={params.context?.appKey}>
+										<Button
+											variant="ghost"
+											onClick={(e) => {
+												e.preventDefault();
+												e.stopPropagation();
 
-                      setSelectedApp(params.data);
-                    }}
-                  >
-                    <Icon data={edit} size={16} />
-                  </Button>
-                  <Button
-                    variant="ghost"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      const appKey = params.data?.appKey;
-                      appKey && deleteAppByAppKey(appKey);
-                    }}
-                  >
-                    <Icon data={delete_to_trash} size={16} />
-                  </Button>
-                </Styles.CellWrapper>
-              );
-            },
-          },
-        ]}
-      />
-    </Styles.Wrapper>
-  );
+												setSelectedApp(params.data);
+											}}
+										>
+											<Icon data={edit} size={16} />
+										</Button>
+										<Button
+											variant="ghost"
+											onClick={(e) => {
+												e.preventDefault();
+												e.stopPropagation();
+												const appKey = params.data?.appKey;
+												appKey && deleteAppByAppKey(appKey);
+											}}
+										>
+											<Icon data={delete_to_trash} size={16} />
+										</Button>
+									</Styles.CellWrapper>
+								);
+							},
+						},
+					]}
+				/>
+			</Styles.TableContent>
+		</AgStyle>
+	);
 };
--- client/apps/portal-administration/src/components/OnboardedApps/OnboardApp.tsx
+++ client/apps/portal-administration/src/components/OnboardedApps/OnboardApp.tsx
@@ -1,126 +1,113 @@
-import {
-  Card,
-  Button,
-  Typography,
-  Autocomplete,
-} from "@equinor/eds-core-react";
+import { Card, Button, Typography, Autocomplete } from '@equinor/eds-core-react';
 
-import { zodResolver } from "@hookform/resolvers/zod";
+import { zodResolver } from '@hookform/resolvers/zod';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { SubmitHandler, useForm } from "react-hook-form";
-import { useEffect } from "react";
-import { OnboardAppInputs, onboardAppInput } from "../../schema/app";
-import {
-  useAddOnboardedApp,
-  useOnboardedApps,
-} from "../../hooks/use-onboarded-apps";
-import { useGetContextTypes } from "../../hooks/use-context-type-query";
-import { AppSelector } from "./AppSelector";
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import { OnboardAppInputs, onboardAppInput } from '../../schema/app';
+import { useAddOnboardedApp, useOnboardedApps } from '../../hooks/use-onboarded-apps';
+import { useGetContextTypes } from '../../hooks/use-context-type-query';
+import { AppSelector } from './AppSelector';
 
 const Style = {
-  Wrapper: styled.div`
-    padding: 1rem;
-    gap: 1rem;
-    display: flex;
-    flex-direction: column;
-  `,
-  Card: styled(Card)`
-    padding: 1rem;
-  `,
-  From: styled.form`
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-  `,
-  ErrorWrapper: styled.div`
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-  `,
-  Heading: styled(Typography)`
-    padding: 0.5rem 0;
-  `,
+	Wrapper: styled.div`
+		gap: 1rem;
+		display: flex;
+		flex-direction: column;
+	`,
+	Card: styled(Card)`
+		padding: 1rem;
+	`,
+	From: styled.form`
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	`,
+	ErrorWrapper: styled.div`
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+	`,
+	Heading: styled(Typography)`
+		padding: 0.5rem 0;
+	`,
 };
 
 export const OnboardApp = () => {
-  const { mutateAsync: createApp, reset: resetCreate } = useAddOnboardedApp();
-  const { data } = useOnboardedApps();
+	const { mutateAsync: createApp, reset: resetCreate } = useAddOnboardedApp();
+	const { data } = useOnboardedApps();
 
-  const { data: contextTypes } = useGetContextTypes();
+	const { data: contextTypes } = useGetContextTypes();
 
-  const {
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    setError,
-    watch,
-    setValue,
-    clearErrors,
-  } = useForm<OnboardAppInputs>({
-    resolver: zodResolver(onboardAppInput),
-    defaultValues: {
-      contextTypes: [],
-    },
-  });
-  const appKey = watch().appKey;
+	const {
+		handleSubmit,
+		formState: { errors, isSubmitting },
+		setError,
+		watch,
+		setValue,
+		clearErrors,
+	} = useForm<OnboardAppInputs>({
+		resolver: zodResolver(onboardAppInput),
+		defaultValues: {
+			contextTypes: [],
+		},
+	});
+	const appKey = watch().appKey;
 
-  useEffect(() => {
-    if (Boolean(data?.find((onboardedApp) => onboardedApp.appKey === appKey))) {
-      setError("appKey", {
-        message: `App with appKey ${appKey} is already onboarded.`,
-      });
-    }
-  }, [data, appKey]);
+	useEffect(() => {
+		if (Boolean(data?.find((onboardedApp) => onboardedApp.appKey === appKey))) {
+			setError('appKey', {
+				message: `App with appKey ${appKey} is already onboarded.`,
+			});
+		}
+	}, [data, appKey]);
 
-  const onSubmit: SubmitHandler<OnboardAppInputs> = async (app) => {
-    await createApp(app);
-    resetCreate();
-    setValue("appKey", "");
-  };
+	const onSubmit: SubmitHandler<OnboardAppInputs> = async (app) => {
+		await createApp(app);
+		resetCreate();
+		setValue('appKey', '');
+	};
 
-  return (
-    <Style.Wrapper>
-      <Style.Card>
-        <Style.Heading variant="h3">Onboard App</Style.Heading>
-        <Style.From onSubmit={handleSubmit(onSubmit)} id="test">
-          <AppSelector
-            errors={errors.appKey}
-            message={errors.appKey?.message}
-            onChange={(app) => {
-              clearErrors("appKey");
-              if (app) {
-                setValue("appKey", app.id);
-              } else {
-                setValue("appKey", "", { shouldDirty: true });
-              }
-            }}
-          />
-          <Autocomplete
-            id="textfield-context-types"
-            multiple
-            variant={errors.contextTypes && "error"}
-            helperText={errors.contextTypes?.message}
-            options={contextTypes?.map((ct) => ct.type) || []}
-            selectedOptions={watch().contextTypes}
-            onOptionsChange={({ selectedItems }) => {
-              setValue("contextTypes", selectedItems);
-            }}
-            itemCompare={(item, compare) => {
-              return item === compare;
-            }}
-            label="Context Types"
-          />
-        </Style.From>
-        <Button
-          form="test"
-          type="submit"
-          disabled={isSubmitting || Boolean(errors.appKey)}
-        >
-          Save
-        </Button>
-      </Style.Card>
-    </Style.Wrapper>
-  );
+	return (
+		<Style.Wrapper>
+			<Style.Card>
+				<Style.Heading variant="h3">Onboard App</Style.Heading>
+				<Style.From onSubmit={handleSubmit(onSubmit)} id="test">
+					<AppSelector
+						errors={errors.appKey}
+						message={errors.appKey?.message}
+						onChange={(app) => {
+							clearErrors('appKey');
+							if (app) {
+								setValue('appKey', app.id);
+							} else {
+								setValue('appKey', '', { shouldDirty: true });
+							}
+						}}
+					/>
+					<Autocomplete
+						id="textfield-context-types"
+						multiple
+						variant={errors.contextTypes && 'error'}
+						helperText={errors.contextTypes?.message}
+						options={contextTypes?.map((ct) => ct.type) || []}
+						selectedOptions={watch().contextTypes}
+						onOptionsChange={({ selectedItems }) => {
+							setValue('contextTypes', selectedItems);
+						}}
+						itemCompare={(item, compare) => {
+							return item === compare;
+						}}
+						label="Context Types"
+					/>
+				</Style.From>
+				<Button form="test" type="submit" disabled={isSubmitting || Boolean(errors.appKey)}>
+					Save
+				</Button>
+			</Style.Card>
+		</Style.Wrapper>
+	);
 };
