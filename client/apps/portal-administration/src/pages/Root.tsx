8aa5278977e3de9cb8274ef47048e2d57755d07d
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-09-06 12:50:15 +0200

    chore: Update Root.tsx to use styled-components for consistent styling

--- client/apps/portal-administration/src/pages/Root.tsx
+++ client/apps/portal-administration/src/pages/Root.tsx
@@ -1,41 +1,38 @@
-import { Outlet, useParams } from "react-router-dom";
-import styled from "styled-components";
-import SideMenu from "../components/SideMenu";
-import { PortalContextComponent } from "../context/PortalContext";
-import { Snack } from "../components/Snack";
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+import SideMenu from '../components/SideMenu';
+import { PortalContextComponent } from '../context/PortalContext';
+import { Snack } from '../components/Snack';
 
 const Styles = {
-  Content: styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-    position: relative;
-  `,
-  Section: styled.section`
-    display: flex;
-
-    height: inherit;
-  `,
-  Wrapper: styled.div`
-    display: block;
-    height: inherit;
-    width: 100%;
-  `,
+	Content: styled.div`
+		flex: 1;
+		position: relative;
+		overflow-y: auto;
+	`,
+	Section: styled.section`
+		display: flex;
+		height: inherit;
+	`,
+	Wrapper: styled.div`
+		display: block;
+		height: inherit;
+		width: 100%;
+	`,
 };
 
 export const Root = () => {
-  return (
-    <Styles.Wrapper>
-      <PortalContextComponent>
-        <Styles.Section>
-          <Snack />
-          <SideMenu />
-          <Styles.Content>
-            <Outlet />
-          </Styles.Content>
-        </Styles.Section>
-      </PortalContextComponent>
-    </Styles.Wrapper>
-  );
+	return (
+		<Styles.Wrapper>
+			<PortalContextComponent>
+				<Styles.Section>
+					<Snack />
+					<SideMenu />
+					<Styles.Content>
+						<Outlet />
+					</Styles.Content>
+				</Styles.Section>
+			</PortalContextComponent>
+		</Styles.Wrapper>
+	);
 };
