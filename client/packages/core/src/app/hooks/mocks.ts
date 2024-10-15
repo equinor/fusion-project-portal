edd42dcf0e35ca81ac0fe3e32777fa7a1a33f325
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-10-15 10:14:42 +0200

    Refactor app loading configuration and mocks

--- client/packages/core/src/app/hooks/mocks.ts
+++ client/packages/core/src/app/hooks/mocks.ts
@@ -3,13 +3,15 @@ import { vi } from 'vitest';
 import { AppConfig, AppManifest, AppModuleProvider } from '@equinor/fusion-framework-module-app';
 import { BehaviorSubject } from 'rxjs';
 
-const config: AppConfig = {
+const config: AppConfig<AppConfigMock> = {
 	environment: {
 		env: 'test',
-		home: '/',
 	},
-	endpoints: {},
 };
+type AppConfigMock = {
+	env: string;
+};
+
 export const getAppConfigMock = vi.fn();
 export const getAppManifestMock = vi.fn();
 export const getAppManifestsMock = vi.fn();
@@ -19,9 +21,9 @@ export const appProvider = new AppModuleProvider({
 		client: {
 			[Symbol.dispose]: vi.fn(),
 
-			getAppConfig: () => {
+			getAppConfig: <A = AppConfigMock>() => {
 				getAppConfigMock();
-				return new BehaviorSubject<AppConfig<any>>(config);
+				return new BehaviorSubject<AppConfig<A>>(config as AppConfig<A>);
 			},
 			getAppManifest: ({ appKey }) => {
 				getAppManifestMock();
--- client/packages/core/src/app/hooks/use-current-app.test.ts
+++ client/packages/core/src/app/hooks/use-current-app.test.ts
@@ -4,42 +4,7 @@ import { act, renderHook } from '@testing-library/react';
 
 import { useCurrentApp } from './use-current-app';
 
-import { AppConfig, AppManifest, AppModuleProvider, IAppClient } from '@equinor/fusion-framework-module-app';
-import { BehaviorSubject } from 'rxjs';
-
-const config: AppConfig = {
-	environment: {
-		env: 'test',
-		home: '/',
-	},
-	endpoints: {},
-};
-export const getAppConfigMock = vi.fn();
-export const getAppManifestMock = vi.fn();
-export const getAppManifestsMock = vi.fn();
-
-export const appProvider = new AppModuleProvider({
-	config: {
-		client: {
-			[Symbol.dispose]: vi.fn(),
-			getAppManifest: ({ appKey }) => {
-				getAppManifestMock();
-				return new BehaviorSubject({
-					appKey: appKey,
-					displayName: 'testName',
-				} as AppManifest);
-			},
-			getAppManifests: () => {
-				getAppManifestsMock();
-				return new BehaviorSubject([] as AppManifest[]);
-			},
-			getAppConfig: () => {
-				getAppConfigMock();
-				return new BehaviorSubject<AppConfig<any>>(config);
-			},
-		},
-	},
-});
+import { appProvider, getAppConfigMock, getAppManifestMock } from './mocks';
 
 beforeEach(() => {
 	appProvider.clearCurrentApp();
@@ -53,20 +18,24 @@ describe('use-current-app', () => {
 		});
 		expect(result.current?.appKey).toEqual('test');
 	});
-	// test('result - loadConfig', () => {
-	// 	const { result } = renderHook(() => useCurrentApp(appProvider));
-	// 	act(() => {
-	// 		appProvider.setCurrentApp('test');
-	// 	});
-	// 	result.current?.loadConfig();
-	// 	expect(getAppConfigMock).toBeCalled();
-	// });
+	test('result - loadConfig', () => {
+		const { result } = renderHook(() => useCurrentApp(appProvider));
+		act(() => {
+			appProvider.setCurrentApp('test');
+		});
+		result.current?.config$.subscribe(() => {
+			expect(getAppConfigMock).toBeCalled();
+		});
+		result.current?.loadConfig();
+	});
 	test('result - loadManifest', () => {
 		const { result } = renderHook(() => useCurrentApp(appProvider));
 		act(() => {
 			appProvider.setCurrentApp('test');
 		});
+		result.current?.manifest$.subscribe(() => {
+			expect(getAppManifestMock).toBeCalled();
+		});
 		result.current?.loadManifest();
-		expect(getAppManifestMock).toBeCalled();
 	});
 });
