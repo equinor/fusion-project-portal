c66ef67e6a1bce7dfad716680b3dde1dbdab1933
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-10-22 08:02:10 +0200

    chore: Update app.config.ts and package.json in legacyAppLoader
    - Remove unused import and code in app.config.ts
    - Update dev:appLoader script in package.json
    - Remove unused components in legacyAppLoader
    
    Closes #issue-number

--- client/apps/legacyAppLoader/app.config.ts
+++ client/apps/legacyAppLoader/app.config.ts
@@ -1,24 +1,22 @@
-import { mergeAppConfigs, defineAppConfig } from '@equinor/fusion-framework-cli';
-export default defineAppConfig((_env, { base }) =>
-	mergeAppConfigs(
-		{},
-		{
-			environment: {
-				endpoints: {
-					client: {
-						baseUri: 'https://backend-fusion-project-portal-test.radix.equinor.com',
-						defaultScopes: ['api://02f3484c-cad0-4d1d-853d-3a9e604b38f3/access_as_user'],
-					},
-					portal: {
-						baseUri: 'https://fusion-s-portal-ci.azurewebsites.net',
-						defaultScopes: ['5a842df8-3238-415d-b168-9f16a6a6031b/.default'],
-					},
-				},
-				fusionEnv: 'ci',
-				loadingText: 'Loading',
-				appKey: 'handover-garden',
-				basename: 'localhost:3000/apps/handover-garden/',
+import { defineAppConfig } from '@equinor/fusion-framework-cli';
+export default defineAppConfig((_env) => ({
+	environment: {
+		endpoints: {
+			client: {
+				baseUri: 'https://backend-fusion-project-portal-test.radix.equinor.com',
+				defaultScopes: ['api://02f3484c-cad0-4d1d-853d-3a9e604b38f3/access_as_user'],
 			},
-		}
-	)
-);
+			portal: {
+				baseUri: 'https://fusion-s-portal-ci.azurewebsites.net',
+				defaultScopes: ['5a842df8-3238-415d-b168-9f16a6a6031b/.default'],
+			},
+			apps: {
+				defaultScopes: ['5a842df8-3238-415d-b168-9f16a6a6031b/.default'],
+			},
+		},
+		fusionEnv: 'ci',
+		loadingText: 'Loading',
+		appKey: 'project-control-and-analysis',
+		basename: 'localhost:3000/apps/project-control-and-analysis/',
+	},
+}));
