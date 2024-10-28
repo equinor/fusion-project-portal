cead188415c8f8c5eb842cbb96b2d34b3326a997
Christopher Berge Hove <christopher.hove@bouvet.no>
2024-10-28 10:29:41 +0100

    feat: Add Portal class for handling portal configuration and app loading

---
+++ client/packages/core/src/modules/portal-config/portal.ts
@@ -0,0 +1,103 @@
+import { BaseConfig, Extensions, PortalConfig, PortalRouter, PortalState } from './types';
+import { Observable, map, take } from 'rxjs';
+
+import { FlowSubject } from '@equinor/fusion-observable';
+import { Actions, actions } from './state/actions';
+import { createState } from './state/create-state';
+import { PortalConfigProvider } from './provider';
+import { filterEmpty } from './utils';
+
+export interface IPortal {
+	state: PortalState;
+	base: BaseConfig;
+	state$: Observable<PortalState>;
+	portalConfig$: Observable<PortalConfig>;
+	portalConfig: PortalConfig;
+	routes$: Observable<PortalRouter>;
+	apps$: Observable<string[]>;
+	apps: string[];
+	getAppsByContext(contextId: string): void;
+	getApps(): void;
+}
+
+export type CurrentPortal = IPortal;
+
+export class Portal implements IPortal {
+	#state: FlowSubject<PortalState, Actions>;
+
+	base: BaseConfig;
+
+	constructor(args: { provider: PortalConfigProvider; base: BaseConfig; initialPortalConfig?: PortalState }) {
+		this.base = args.base;
+		this.#state = createState(args.provider, args.initialPortalConfig);
+		this.initialize();
+	}
+
+	protected async initialize(): Promise<void> {
+		this.#state.next(actions.fetchPortal(this.base));
+	}
+
+	getAppsByContext(contextId: string): void {
+		this.#state.next(actions.fetchAppsByContextId({ contextId }, true));
+	}
+
+	getApps(): void {
+		this.#state.next(actions.fetchApps());
+	}
+
+	get routes$(): Observable<PortalRouter> {
+		return this.#state.pipe(
+			map(({ routes }) => routes),
+			filterEmpty(),
+			take(1)
+		);
+	}
+
+	get apps$(): Observable<string[]> {
+		return this.#state.pipe(
+			map(({ apps }) => apps),
+			filterEmpty()
+		);
+	}
+	get apps(): string[] {
+		return this.#state.value.apps || [];
+	}
+
+	get extensions$(): Observable<Extensions> {
+		return this.#state.pipe(
+			map(({ extensions }) => extensions),
+			filterEmpty()
+		);
+	}
+
+	get portalConfig$(): Observable<PortalConfig> {
+		return this.#state.pipe(
+			map(({ portal }) => portal),
+			filterEmpty(),
+			take(1)
+		);
+	}
+
+	get portalConfig(): PortalConfig {
+		return this.#state.value.portal;
+	}
+
+	get state(): PortalState {
+		return this.#state.value;
+	}
+
+	get state$(): Observable<PortalState> {
+		return this.#state.pipe(
+			map((state) => state),
+			filterEmpty()
+		);
+	}
+
+	[Symbol.dispose]() {
+		this.complete();
+	}
+
+	public complete() {
+		this.#state.complete();
+	}
+}
