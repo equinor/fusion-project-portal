630c811da204b503ec9283b5c1a4117f872fc693
Kjetil Haugland <khaugl@equinor.com>
2024-10-29 09:27:37 +0100

    Add profile controller

---
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Equinor.ProjectExecutionPortal.WebApi.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Equinor.ProjectExecutionPortal.WebApi.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiVersion("0.1")]
+    [Route("api/profile")]
+    public class ProfileController : ApiControllerBase
+    {
+        [HttpOptions("admin")]
+        [Authorize(Policy = Policies.ProjectPortal.Admin)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public IActionResult Options()
+        {
+            return Ok();
+        }
+    }
+}
