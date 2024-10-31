8420dc3a823a5bf3e5bee28446058ce220d51700
Kjetil Haugland <khaugl@equinor.com>
2024-10-31 10:23:53 +0100

    Minor refactoring

--- backend/src/Equinor.ProjectExecutionPortal.Application/Infrastructure/Mappings/MappingProfile.cs
+++ backend/src/Equinor.ProjectExecutionPortal.Application/Infrastructure/Mappings/MappingProfile.cs
@@ -22,7 +22,7 @@ public class MappingProfile : Profile
             var methodInfo = type.GetMethod("Mapping")
                 ?? type.GetInterface("IMapFrom`1")!.GetMethod("Mapping");
 
-            methodInfo?.Invoke(instance, new object[] { this });
+            methodInfo?.Invoke(instance, [this]);
 
         }
     }
