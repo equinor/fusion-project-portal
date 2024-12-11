07e1e5202b8ff8c10f97d88cff563dbe3a3902a2
Kjetil Haugland <khaugl@equinor.com>
2024-12-11 15:07:52 +0100

    Fix null warnings

--- backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/ApiControllerBase.cs
+++ backend/src/Equinor.ProjectExecutionPortal.WebApi/Controllers/ApiControllerBase.cs
@@ -7,8 +7,8 @@ namespace Equinor.ProjectExecutionPortal.WebApi.Controllers;
 [ApiController]
 public abstract class ApiControllerBase : Controller
 {
-    private ISender _mediator = null!;
-    private IFusionContextResolver _contextResolver = null!;
+    private ISender? _mediator;
+    private IFusionContextResolver? _contextResolver;
 
     protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
     protected IFusionContextResolver ContextResolver => _contextResolver ??= HttpContext.RequestServices.GetRequiredService<IFusionContextResolver>();
