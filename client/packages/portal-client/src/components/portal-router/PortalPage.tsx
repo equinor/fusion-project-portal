cffa892ca233811ed2fc70551590d05fa82ba4a4
Christopher Berge Hove <chbh@equinor.com>
2025-03-12 13:20:55 +0100

    refactor: simplify PortalPage component by removing feature flag checks (#917)
    Co-authored-by: Noggling <Noggling@users.noreply.github.com>

--- client/packages/portal-client/src/components/portal-router/PortalPage.tsx
+++ client/packages/portal-client/src/components/portal-router/PortalPage.tsx
@@ -1,53 +1,16 @@
 import { PortalRoute } from '@portal/core';
-import { FacilityPage, ProjectPage, ProjectPortalPage } from '@equinor/portal-pages';
 import { LoadPage } from './LoadPage';
-import { useFeature } from '@equinor/fusion-framework-react-app/feature-flag';
 
 export const PortalPage = (prop: { route?: Partial<PortalRoute> }) => {
-	const { feature } = useFeature('landing-page');
 	switch (prop.route?.pageKey) {
 		case 'project-portal':
 			return (
-				<>
-					{feature?.enabled ? (
-						<LoadPage
-							pageKey="project-portal-landingpage"
-							path={prop.route.path}
-							messages={prop.route.messages}
-						/>
-					) : (
-						<ProjectPortalPage />
-					)}
-				</>
+				<LoadPage pageKey="project-portal-landingpage" path={prop.route.path} messages={prop.route.messages} />
 			);
 		case 'project':
-			return (
-				<>
-					{feature?.enabled ? (
-						<LoadPage
-							pageKey="project-landingpage"
-							path={prop.route?.path}
-							messages={prop.route?.messages}
-						/>
-					) : (
-						<ProjectPage />
-					)}
-				</>
-			);
+			return <LoadPage pageKey="project-landingpage" path={prop.route?.path} messages={prop.route?.messages} />;
 		case 'facility':
-			return (
-				<>
-					{feature?.enabled ? (
-						<LoadPage
-							pageKey="facility-landingpage"
-							path={prop.route?.path}
-							messages={prop.route?.messages}
-						/>
-					) : (
-						<FacilityPage />
-					)}
-				</>
-			);
+			return <LoadPage pageKey="facility-landingpage" path={prop.route?.path} messages={prop.route?.messages} />;
 		default:
 			return <LoadPage {...prop.route} />;
 	}
